0ed5ead13bc26ccae7090781f779f4da6105a328
Nihaal Sangha <nihaal.git@gmail.com>
2021-02-27 16:34:34 +0000

    Switch to chakra.input

--- thunderstore-components/src/components/FileUpload.tsx
+++ thunderstore-components/src/components/FileUpload.tsx
@@ -1,7 +1,8 @@
+import { chakra } from "@chakra-ui/react";
 import React from "react";
 
 export const FileUpload: React.FC<React.HTMLAttributes<HTMLInputElement>> = ({
   ...props
 }) => {
-  return <input type="file" name="file" {...props} />;
+  return <chakra.input type="file" name="file" {...props} />;
 };
