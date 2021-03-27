2e6a23871e156b981358452c767512cd7f76dfeb
Nihaal Sangha <nihaal.git@gmail.com>
2021-03-27 14:07:21 +0000

    Add sessionid support to apiFetch

--- thunderstore-components/src/fetch.ts
+++ thunderstore-components/src/fetch.ts
@@ -1,15 +1,21 @@
+import Cookies from "js-cookie";
 import { ThunderstoreProviderProps } from "./components/ThunderstoreProvider";
+import { isDevelopment } from "./constants";
 
 export const apiFetch = async (
   context: ThunderstoreProviderProps,
   endpoint: string,
   fetchOptions: RequestInit = {}
 ): Promise<Response> => {
+  const headers = new Headers(fetchOptions.headers);
+
   const apiToken = localStorage.getItem("apiToken");
-  if (apiToken !== null) {
-    const headers = new Headers(fetchOptions.headers);
+  const sessionToken = Cookies.get("sessionid");
+  if (isDevelopment && apiToken) {
     headers.append("Authorization", `Bearer ${apiToken}`);
-    fetchOptions.headers = headers;
+  } else if (sessionToken !== null) {
+    headers.append("Authorization", `Session ${sessionToken}`);
   }
+  fetchOptions.headers = headers;
   return await fetch(context.apiUrl + endpoint.substring(1), fetchOptions);
 };
