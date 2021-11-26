ee0a27f7b6c881c9808bb76fbe2762a89fb9f621
Antti Mäki <antti@anttimaki.com>
2021-11-26 16:31:30 +0200

    Fix Markdown component
    Introducing Preconstruct caused some weird issues. Whenever
    "react-markdown" or "remark-gfm" is imported in a normal way, the app
    crashes with "Error: Element type is invalid: expected a string (for
    built-in components) or a class/function (for composite components) but
    got: undefined". Furthermore, the error only occurs after a full page
    refresh - regular imports don't cause problems when changes are updated
    by live-reload.
    
    The same error occurs with the package versions used before this PR as
    well as with the latest ones.

--- packages/components/src/components/Markdown.tsx
+++ packages/components/src/components/Markdown.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactMarkdown from "react-markdown";
-import gfm from "remark-gfm";
 import ChakraUIRenderer from "chakra-ui-markdown-renderer";
 
 interface MarkdownProps {
@@ -8,9 +6,28 @@ interface MarkdownProps {
 }
 
 export const Markdown: React.FC<MarkdownProps> = ({ children }) => {
-  return (
-    <ReactMarkdown plugins={[gfm]} components={ChakraUIRenderer()}>
-      {children}
-    </ReactMarkdown>
-  );
+  const [markup, setMarkup] = React.useState<JSX.Element>();
+
+  /*
+   * react-markdown and remark-gfm are imported asynchronously because
+   * regular imports result in server error: "Element type is invalid:
+   * expected a string (for built-in components) or a class/function
+   * (for composite components) but got: undefined" ever since
+   * Preconstruct was introduced.
+   */
+  React.useEffect(() => {
+    const generateMarkup = async () => {
+      const { default: ReactMarkdown } = await import("react-markdown");
+      const { default: gfm } = await import("remark-gfm");
+
+      setMarkup(
+        <ReactMarkdown plugins={[gfm]} components={ChakraUIRenderer()}>
+          {children}
+        </ReactMarkdown>
+      );
+    };
+    generateMarkup();
+  }, [children, setMarkup]);
+
+  return markup ?? null;
 };
