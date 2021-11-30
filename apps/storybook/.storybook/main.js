86d5c542c8b38d6fb0799c6bd6a29cc714cf9209
Antti Mäki <antti@anttimaki.com>
2021-11-30 17:00:41 +0200

    Add Chakra UI add-on for Storybook
    Wraps each Storybook story with ChakraProvider, making the expected
    styles work.

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -5,7 +5,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@snek-at/storybook-addon-chakra-ui",  // Must come after @storybook addons
   ],
   "framework": "@storybook/react",
 };
