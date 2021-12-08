1269dbd55a41fe081d2f689a402323f4cc8a8a4c
Antti Mäki <antti@anttimaki.com>
2021-12-08 16:05:47 +0200

    Implement LinkingProvider for @thunderstore/storybook
    This is required so the links used in @thunderstore/components get
    rendered properly in Storybook's showcases.
    
    In the showcases the links are rendered as Chakra's Link (for style
    purposes) wrapping a span element. This way clicking the links doesn't
    lead the user to non-existent paths.

---
+++ apps/storybook/LinkLibrary.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+import {
+  Link as ChakraLink,
+  LinkProps as ChakraLinkProps,
+} from "@chakra-ui/react";
+import {
+  LinkLibrary,
+  thunderstoreLinkProps,
+  ThunderstoreLinkProps,
+} from "@thunderstore/components";
+
+interface LinkProps extends ChakraLinkProps, ThunderstoreLinkProps {
+  url: string;
+}
+
+const Link = (props: LinkProps): React.ReactElement => {
+  const { url, children, ...chakraProps } = props;
+
+  for (const key in thunderstoreLinkProps) {
+    delete chakraProps[key as keyof ThunderstoreLinkProps];
+  }
+
+  // Render links as spans so clicking them does nothing.
+  return (
+    <ChakraLink href={url} {...chakraProps} as="span">
+      {children}
+    </ChakraLink>
+  );
+};
+
+const library: LinkLibrary = {
+  Anonymous: (p) => Link(p),
+  Index: (p) => Link({ ...p, url: "/" }),
+  Package: (p) => Link({ ...p, url: `/c/${p.community}/p/${p.package}` }),
+};
+
+export { library as LinkLibrary };
