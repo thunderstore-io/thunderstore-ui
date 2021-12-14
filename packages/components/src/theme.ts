68ce34ccb934fcce31ce175a6d2866fd682d9f5d
Antti Mäki <antti@anttimaki.com>
2021-12-14 14:13:37 +0200

    Set basic theme styles via Chakra's theme
    * DRY the color definitions
    * Set max-width and some padding for the page
    * Set the default font
    * Set the body's background color
    * Remove focus styles from links. The default provided by Chakra UI was
      quite gaudy. We might want to switch using some milder version later
      on.
    
    Refs TS-189

--- packages/components/src/theme.ts
+++ packages/components/src/theme.ts
@@ -1,10 +1,36 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const blue = "#242e48";
+const darkBlue = "#17213b";
+const lightBlue = "#353e58";
+
 export const theme = extendTheme({
-  components: {
-    Link: {
-      baseStyle: {
-        color: "teal.500",
+  colors: {
+    ts: {
+      blue,
+      darkBlue,
+      lightBlue,
+    },
+  },
+  fonts: {
+    body: "Exo2",
+    heading: "Raleway",
+  },
+  styles: {
+    global: {
+      a: {
+        _focus: {
+          boxShadow: "none !important",
+        },
+      },
+      body: {
+        maxWidth: "1180px",
+        margin: "0 auto !important",
+        padding: "0 20px",
+        font: "500 16px/1.2 Exo2, sans-serif",
+      },
+      "html, body": {
+        backgroundColor: blue,
       },
     },
   },
