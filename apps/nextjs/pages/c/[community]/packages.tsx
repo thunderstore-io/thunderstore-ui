940c3e7b2fe98c71ad972d00ef09c13b7c0fb913
Antti Mäki <antti@anttimaki.com>
2022-03-22 14:09:19 +0200

    Move dummy data from page definitions to separate files

--- apps/nextjs/pages/c/[community]/packages.tsx
+++ apps/nextjs/pages/c/[community]/packages.tsx
@@ -22,6 +22,7 @@ import {
   queryToStrs,
   useQueryToState,
 } from "hooks/useQueryToState";
+import { fakeCategories, fakeData } from "placeholder/packageListing";
 import { Category, categoriesToSelectOptions } from "utils/transforms/category";
 import {
   BackendPackageCard,
@@ -287,7 +288,7 @@ const getFakeData = (
 
   return {
     bg_image_src: "https://api.lorem.space/image/game?w=2000&h=200",
-    categories: getFakeCategories(),
+    categories: fakeCategories,
     community_name: communityIdentifier,
     packages,
   };
@@ -307,43 +308,11 @@ const getFakeDataPackages = (
   nsfw: boolean,
   deprecated: boolean
 ): BackendPackageCard[] => {
-  let packages = [
-    ...Array(30)
-      .fill(0)
-      .map((_x, i) => ({
-        categories:
-          i % 4 === 0
-            ? [
-                { slug: "items", name: "Items" },
-                { slug: "tweaks", name: "Tweaks" },
-                { slug: "mods", name: "Mods" },
-              ]
-            : i % 4 === 1
-            ? [
-                { slug: "items", name: "Items" },
-                { slug: "tweaks", name: "Tweaks" },
-              ]
-            : i % 4 === 2
-            ? [{ slug: "items", name: "Items" }]
-            : [],
-        community_name: communityIdentifier,
-        community_identifier: communityIdentifier,
-        description:
-          "Adds item drop on killing the Shopkeeper, and several new items.",
-        download_count: 40,
-        image_src:
-          i === 0 || i % 5
-            ? "https://api.lorem.space/image/game?w=266&h=200"
-            : null,
-        is_deprecated: i !== 0 && i % 8 === 0,
-        is_nsfw: i !== 0 && i % 6 === 0,
-        is_pinned: i < 3,
-        last_updated: "2021-12-17T15:00:00Z",
-        rating_score: 600,
-        package_name: "NewtDrop",
-        team_name: "BoneCapTheTweet",
-      })),
-  ];
+  let packages = fakeData.map((p) => ({
+    ...p,
+    community_name: communityIdentifier,
+    community_identifier: communityIdentifier,
+  }));
 
   if (query !== "") {
     const q = query.toLowerCase();
@@ -378,21 +347,3 @@ const getFakeDataPackages = (
 
   return packages;
 };
-
-/**
- * TODO: Categories should be fetched from the backend, either with the
- * same request as the first batch of packages, or in a separate request.
- */
-const getFakeCategories = () => [
-  { slug: "items", name: "Items" },
-  { slug: "tweaks", name: "Tweaks" },
-  { slug: "mods", name: "Mods" },
-  { slug: "tools", name: "Tools" },
-  { slug: "maps", name: "Maps" },
-  { slug: "skins", name: "Skins" },
-  { slug: "audio", name: "Audio" },
-  { slug: "player-characters", name: "Player Characters" },
-  { slug: "client-side", name: "Client-side" },
-  { slug: "server-side", name: "Server-side" },
-  { slug: "language", name: "Language" },
-];
--- apps/nextjs/pages/index.tsx
+++ apps/nextjs/pages/index.tsx
@@ -4,6 +4,7 @@ import { GetServerSideProps } from "next";
 
 import { HalfPageBackground } from "components/Background";
 import { ContentWrapper } from "components/Wrapper";
+import { fakeData } from "placeholder/frontpage";
 import { communityCardToProps } from "utils/transforms/communityCard";
 
 interface PageProps {
@@ -95,68 +96,4 @@ const HighlighBox: React.FC<HighlighProps> = (props) => {
       </Text>
     </Box>
   );
-};
-
-const bg_image_src = "https://api.lorem.space/image/game?w=356&h=110";
-const fakeData = {
-  communities: [
-    {
-      download_count: 40000,
-      bg_image_src,
-      identifier: "riskofrain2",
-      package_count: 600,
-      name: "Risk of Rain 2",
-    },
-    {
-      download_count: 1408576,
-      bg_image_src,
-      identifier: "h3vr",
-      package_count: 2048,
-      name: "H3VR",
-    },
-    {
-      download_count: 100000000,
-      bg_image_src,
-      identifier: "valheim",
-      package_count: 10000000,
-      name: "Valheim",
-    },
-    {
-      download_count: 0,
-      bg_image_src,
-      identifier: "boneworks",
-      package_count: 0,
-      name: "BONEWORKS",
-    },
-    {
-      download_count: 23232,
-      bg_image_src: null,
-      identifier: "gtfo",
-      package_count: 23,
-      name: "GTFO",
-    },
-    {
-      download_count: 1000,
-      bg_image_src,
-      identifier: "outward",
-      package_count: 1,
-      name: "Outward",
-    },
-    {
-      download_count: 123456789,
-      bg_image_src,
-      identifier: "rounds",
-      package_count: 1024,
-      name: "ROUNDS",
-    },
-    {
-      download_count: 128,
-      bg_image_src,
-      identifier: "talespire",
-      package_count: 3,
-      name: "Talespire",
-    },
-  ],
-  download_count: 15215000,
-  package_count: 5000,
 };
