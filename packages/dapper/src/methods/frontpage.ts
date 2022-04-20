4b7be00ea6496e41f659b49ad4a0dac18e63ada2
Antti Mäki <antti@anttimaki.com>
2022-04-20 13:47:02 +0300

    @thunderstore/dapper: add support for frontpage
    Refs TS-320

---
+++ packages/dapper/src/methods/frontpage.ts
@@ -0,0 +1,48 @@
+import { CommunityCardProps } from "@thunderstore/components";
+import { z } from "zod";
+
+import { Dapper } from "../dapper";
+
+// Schema describing the data received from backend, used to validate the data.
+const schema = z.object({
+  communities: z.array(
+    z.object({
+      bg_image_src: z.nullable(z.string()),
+      download_count: z.number().min(0),
+      identifier: z.string().min(1),
+      name: z.string().min(1),
+      package_count: z.number().min(0),
+    })
+  ),
+  download_count: z.number().min(0),
+  package_count: z.number().min(0),
+});
+
+// Define values returned by the Dapper method.
+interface Frontpage {
+  communities: CommunityCardProps[];
+  downloadCount: number;
+  packageCount: number;
+}
+
+// Method for transforming the received data to a format that will be
+// passed on.
+const transform = (viewData: z.infer<typeof schema>): Frontpage => ({
+  communities: viewData.communities.map((c) => ({
+    downloadCount: c.download_count,
+    identifier: c.identifier,
+    imageSrc: c.bg_image_src,
+    name: c.name,
+    packageCount: c.package_count,
+  })),
+  downloadCount: viewData.download_count,
+  packageCount: viewData.package_count,
+});
+
+// Dapper method type, defining the parameters required to fetch the data.
+export type GetFrontpage = () => Promise<Frontpage>;
+
+// Method implementation for Dapper class.
+export const getFrontpage: GetFrontpage = async function (this: Dapper) {
+  return await this.queryAndProcess("frontpage/", [], schema, transform);
+};
