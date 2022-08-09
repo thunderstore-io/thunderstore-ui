ff3d9fdaa239b4a87468229f57669791e6473d60
Mythic <mythic@thunderstore.io>
2022-08-09 22:18:06 +0300

    Add basic functionality to mod select dropdown
    Add some very basic functionality to the mod select dropdown,
    specifically allow for selection & de-selection of mods in the mod
    select modal.
    
    At this stage the dropdown & modal still have some control & layout
    issues.

--- apps/sertra/components/mod-selector/iconButton.tsx
+++ apps/sertra/components/mod-selector/iconButton.tsx
@@ -1,8 +1,20 @@
 import styles from "./iconButton.module.css";
+import { DetailedHTMLProps } from "react";
 
 interface IconButtonProps {
   content: string;
+  buttonProps?: DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >;
 }
-export const IconButton: React.FC<IconButtonProps> = ({ content }) => {
-  return <button className={styles.headerCloseButton}>{content}</button>;
+export const IconButton: React.FC<IconButtonProps> = ({
+  content,
+  buttonProps,
+}) => {
+  return (
+    <button {...buttonProps} className={styles.headerCloseButton}>
+      {content}
+    </button>
+  );
 };
