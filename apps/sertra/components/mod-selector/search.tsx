4d3bfc891cc17d45439ce04e81b11ce6e3797e90
Mythic <mythic@thunderstore.io>
2022-08-09 21:54:10 +0300

    Implement mod select dropdown layout
    Implement the mod select dropdown layout & styles

--- apps/sertra/components/mod-selector/search.tsx
+++ apps/sertra/components/mod-selector/search.tsx
@@ -1,23 +1,57 @@
-import { PropsWithChildren } from "react";
+import { ReactNode, useState } from "react";
 import styles from "./search.module.css";
 
-interface SearchBoxProps {
+interface SearchBoxProps<T> {
   placeholder?: string;
+  renderOption: (option: T) => ReactNode;
+  keyExtractor: (option: T) => string | number;
+  options: T[];
 }
 
-export const SearchBox: React.FC<PropsWithChildren<SearchBoxProps>> = ({
+export const SearchBox = <T,>({
   placeholder,
-}) => {
+  renderOption,
+  keyExtractor,
+  options,
+}: SearchBoxProps<T>) => {
+  const [isFocused, setFocused] = useState<boolean>(false);
+
   return (
-    <div className={styles.searchBox}>
-      <div className={styles.searchIcon}>🔍</div>
-      <div className={styles.searchInputArea}>
-        <input
-          className={styles.searchInput}
-          type={"text"}
-          placeholder={placeholder}
-        />
+    <div
+      className={styles.searchField}
+      onFocus={() => setFocused(true)}
+      onBlur={(e) => {
+        if (!e.currentTarget.contains(e.relatedTarget)) {
+          setFocused(false);
+        }
+      }}
+    >
+      <div className={styles.searchBox}>
+        <div className={styles.searchIcon}>🔍</div>
+        <div className={styles.searchInputArea}>
+          <input
+            className={styles.searchInput}
+            type={"text"}
+            placeholder={placeholder}
+          />
+        </div>
       </div>
+      {isFocused && (
+        <div className={styles.optionsAnchor}>
+          <div className={styles.optionsBox}>
+            {options.map((option) => {
+              return (
+                <button
+                  className={styles.optionButton}
+                  key={keyExtractor(option)}
+                >
+                  {renderOption(option)}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
