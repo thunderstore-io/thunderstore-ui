ef7633265e645cae4b97272d3159f94ec4fc6d75
Antti Mäki <antti@anttimaki.com>
2022-08-22 10:27:46 +0300

    @thunderstore/sertra: fix footer layout
    * On short pages footer sticks to the bottom of the viewport
    * On long pages footer is part of document flow, i.e. only visible
      when page is scrolled to the end
    * Footer scales sensibly on narrower displays instead of being fixed
      width. The expected behaviour isn't specified, so the specifics
      might change in the future
    
    Refs TS-678

--- apps/sertra/components/footer.tsx
+++ apps/sertra/components/footer.tsx
@@ -1,40 +1,12 @@
 import { FC, PropsWithChildren } from "react";
 
-export const Footer: FC<PropsWithChildren> = () => {
-  return (
-    <footer style={{ backgroundColor: "var(--color-darkblue)" }}>
-      <div style={{ width: "1140px", marginLeft: "auto", marginRight: "auto" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            fontFamily: "var(--font-family-body)",
-          }}
-        >
-          <h2
-            style={{
-              flex: 1,
-              fontSize: "14px",
-              color: "var(--color-coolgray)",
-              marginTop: "19px",
-              marginBottom: "19px",
-            }}
-          >
-            Thunderstore 2022
-          </h2>
-          <div
-            style={{
-              fontSize: "14px",
-              color: "var(--color-coolgray)",
-              marginTop: "19px",
-              marginBottom: "19px",
-            }}
-          >
-            <a href={"https://discord.com/invite/UWpWhjZken"}>Discord</a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
-};
+import styles from "./footer.module.css";
+
+export const Footer: FC<PropsWithChildren> = () => (
+  <footer className={styles.container}>
+    <div className={styles.content}>
+      <h2 className={styles.service}>Thunderstore 2022</h2>
+      <a href="https://discord.com/invite/UWpWhjZken">Discord</a>
+    </div>
+  </footer>
+);
