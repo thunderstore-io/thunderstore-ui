79b4f275465626abb6bf186f1791b0b78db01108
Antti Mäki <antti@anttimaki.com>
2022-09-02 12:24:41 +0300

    @thunderstore/sertra: fix imports
    * Add Font Awesome stuff to dependencies
    * Fix broken relative imports
    
    Refs TS-764

--- apps/sertra/pages/_app.tsx
+++ apps/sertra/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Layout } from "../components/layout";
+import { Layout } from "../components/Layout";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 import { faIceCream } from "@fortawesome/free-solid-svg-icons";
