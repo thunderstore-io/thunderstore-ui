03279892ea8d09b88d8ce8b57d21661419c6f037
Antti Mäki <antti@anttimaki.com>
2022-09-07 13:15:46 +0300

    @thunderstore/sertra: make SVGs reusable
    * Add svgr library to allow importing SVGs as React components
    * Remove hardcoded fill color from ts-logo.svg so it can be adjusted
    * Document how SVGs can be used on TSX and CSS files
    
    Refs TS-690

--- apps/sertra/next.config.js
+++ apps/sertra/next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: {
+        loader: "@svgr/webpack",
+        options: {
+          prettier: false,
+          svgo: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: "preset-default",
+                params: {
+                  overrides: { removeViewBox: false },
+                },
+              },
+            ],
+          },
+          titleProp: true,
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
