a22450de967a9ab9a2a99060fd15153dc0de051e
Oksamies <asko.soisalo@gmail.com>
2022-09-08 22:07:21 +0300

    Add useEffect to PackageDataFetcher
    That and a bit of typing since it was required by the useEffect
    
    refs TS-712

--- apps/sertra/api/models.ts
+++ apps/sertra/api/models.ts
@@ -14,12 +14,44 @@ export interface ServerListingData extends ServerListingBase {
   mod_count: number;
 }
 
+export interface TSListingModVersions {
+  name: string;
+  full_name: string;
+  description: string | null;
+  icon: string | null; // This is an url
+  version_number: string;
+  dependencies: [];
+  download_url: string;
+  downloads: number;
+  date_created: string;
+  website_url: string | null;
+  is_active: boolean;
+  uuid4: string;
+  file_size: number;
+}
+
+export interface TSListingMod {
+  name: string;
+  full_name: string;
+  owner: string | null;
+  package_url: string;
+  date_created: string;
+  date_updated: string;
+  uuid: string;
+  rating_score: number;
+  is_pinned: boolean;
+  is_deprecated: boolean;
+  has_nsfw_content: boolean;
+  versions: TSListingModVersions[];
+  categories: [];
+}
+
 export interface ListingMod {
   name: string;
-  owner: string;
-  description: string;
-  version: string;
-  icon_url: string;
+  owner: string | null;
+  description: string | null;
+  version: string | null;
+  icon_url: string | null;
 }
 
 export interface ServerListingDetailData extends ServerListingBase {
--- apps/sertra/components/PackageDataFetcher.tsx
+++ apps/sertra/components/PackageDataFetcher.tsx
@@ -1,21 +1,47 @@
+import { useEffect, useState } from "react";
+import { ListingMod, TSListingMod } from "../api/models";
 import { ApiURLs, TsApiURLs } from "../api/urls";
 
+// TODO: To be replaced with ServerListingDetailData when the listing will contain an actual
+// object instead of an list of strings
+
+interface TempModRefType {
+  mods: string[];
+}
+
 export const FetchListingData = async (listingId: string) => {
-  const res = await fetch(ApiURLs.ServerDetail(listingId));
-  const mods_res = await fetch(TsApiURLs.V1Packages("v-rising"));
-  const data = await res.json();
-  const mods_data = await mods_res.json();
+  const [listing, setListing] = useState<TempModRefType>();
+  useEffect(() => {
+    async function fetchListing() {
+      return await fetch(ApiURLs.ServerDetail(listingId));
+    }
+    async function jsonifyListing() {
+      setListing(await (await fetchListing()).json());
+    }
+    jsonifyListing();
+  });
+
+  const [mods, setMods] = useState<TSListingMod[]>([]);
+  useEffect(() => {
+    async function fetchMods() {
+      return await fetch(TsApiURLs.V1Packages("v-rising"));
+    }
+    async function jsonifyMods() {
+      setMods(await (await fetchMods()).json());
+    }
+    jsonifyMods();
+  });
 
   const updated_listing_mods_data = [];
-  for (const mod_ref of data.mods) {
-    const new_mod_data = {
+  for (const mod_ref of listing.mods) {
+    const new_mod_data: ListingMod = {
       name: "MOD DETAIL FETCH FAILED: " + mod_ref,
       owner: null,
       version: null,
       icon_url: null,
       description: null,
     };
-    for (const mod of mods_data) {
+    for (const mod of mods) {
       if (mod_ref.startsWith(mod.full_name)) {
         new_mod_data.name = mod.name;
         new_mod_data.owner = mod.owner;
