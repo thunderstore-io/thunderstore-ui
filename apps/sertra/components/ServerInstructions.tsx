2ab76515a72d0fc59ac73c3c3468512ba39ffc36
Vili Manninen <vili.manninen@riskidev.fi>
2022-09-12 11:12:44 +0300

    Better help text on server details

--- apps/sertra/components/ServerInstructions.tsx
+++ apps/sertra/components/ServerInstructions.tsx
@@ -5,10 +5,17 @@ export const ServerInstructions: React.FC = () => (
   <section>
     <h2 className={pageStyles.sectionTitle}>How To Play</h2>
     <ol className={styles.instructions}>
-      <li>Click butan</li>
-      <li>Öpens TMM</li>
-      <li>Sync Mods</li>
-      <li>Enjoy</li>
+      <li>
+        Install Thunderstore{" "}
+        <a href="https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager">
+          Mod Manager
+        </a>
+      </li>
+      <li>Click the join server button to launch Mod Manager</li>
+      <li>Sync mods and launch the game in Mod manager</li>
+      <li>Copy the connection address</li>
+      <li>Use the address in-game to join the server</li>
+      <li>Enjoy gaming on modded servers!</li>
     </ol>
   </section>
 );
