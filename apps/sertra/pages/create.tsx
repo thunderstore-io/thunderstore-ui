9970ebbe58ba830ea3d4e2a6c274f24048b4247c
Antti Mäki <antti@anttimaki.com>
2022-09-19 13:01:01 +0300

    @thunderstore/sertra: code cleanup for server submit view
    * Remove unnecessary wrapper divs
    * Remove unnecessary style declarations
    * Remove duplicate selectors
    * Order declarations within declaration blocks to more logical order
    * Order declaration blocks to match the order of markup
    * Fix some minor style issues, e.g. the width and alignment of labels
    
    The modal component, which was not present during the original PR was
    left out of this ticket's scope. See TS-1060 for more info.
    
    Refs TS-565

--- apps/sertra/pages/create.tsx
+++ apps/sertra/pages/create.tsx
@@ -44,7 +44,7 @@ const SubmitServer: NextPage<{ swrFallback: { [key: string]: unknown } }> = ({
       connection_data: data.connectionInfo,
       // TODO: Populate this with mod ids etc
       mods: ["1", "2", "3"],
-      is_pvp: data.mode === "pvp" ? true : false,
+      is_pvp: data.mode === "pvp",
       requires_password: data.isPasswordProtected,
     };
     fetch(ApiURLs.ServerCreate, {
@@ -57,80 +57,77 @@ const SubmitServer: NextPage<{ swrFallback: { [key: string]: unknown } }> = ({
   return (
     <SWRConfig value={{ fallback: swrFallback }}>
       <ModSelectorModal />
-      <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.formHeader}>
-          <div className={styles.formTitle}>Submit Server</div>
-        </div>
-        <div className={styles.form}>
-          <div className={styles.row}>
-            <label htmlFor={"game"}>Game</label>
-            <select id={"game"} className={styles.input}>
-              <option value={"v-rising"}>V Rising</option>
-            </select>
-          </div>
-          <div className={styles.row}>
-            <label htmlFor={"serverName"}>Server Name</label>
-            <input
-              className={styles.input}
-              id={"serverName"}
-              type={"text"}
-              {...register("serverName", { required: true })}
-            />
-            {/* TODO: Add styling here */}
-            {errors.serverName?.type === "required" &&
-              "Server name is required"}
-          </div>
-          <div className={styles.row}>
-            <label htmlFor={"connectionInfo"}>Connection Info</label>
-            <input
-              className={styles.input}
-              id={"connectionInfo"}
-              type={"text"}
-              {...register("connectionInfo", { required: true })}
-            />
-            {/* TODO: Add styling here */}
-            {errors.connectionInfo?.type === "required" &&
-              "Connection info is required"}
-          </div>
-          <div className={styles.row}>
-            <label htmlFor={"description"}>Description</label>
-            <textarea
-              className={styles.textarea}
-              id={"description"}
-              {...register("description")}
-            />
-          </div>
-          <div className={styles.row}>
-            <label htmlFor={"mode"}>Mode</label>
-            <select className={styles.input} id={"mode"} {...register("mode")}>
-              <option value={"pvp"}>PvP</option>
-              <option value={"pve"}>PvE</option>
-            </select>
-          </div>
-          <div className={styles.row}>
-            <label htmlFor={"mods"}>Mods</label>
-            <select className={styles.input} id={"mods"} {...register("mods")}>
-              {/*TODO: Populate*/}
-            </select>
-          </div>
-          <div className={styles.row}>
-            <label htmlFor={"isPasswordProtected"}>Password Protected</label>
-            <div className={styles.checkboxContainer}>
-              <input
-                className={styles.checkbox}
-                id={"isPasswordProtected"}
-                type={"checkbox"}
-                {...register("isPasswordProtected")}
-              />
-            </div>
-          </div>
-        </div>
+
+      <h1 className={styles.formHeader}>Submit Server</h1>
+
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.row}>
-          <div className={styles.label} />
-          <button id={"submit"} className={styles.submit}>
-            Submit
-          </button>
+          <label htmlFor={"game"}>Game</label>
+          <select id={"game"} className={styles.input}>
+            <option value={"v-rising"}>V Rising</option>
+          </select>
         </div>
+
+        <div className={styles.row}>
+          <label htmlFor={"serverName"}>Server Name</label>
+          <input
+            className={styles.input}
+            id={"serverName"}
+            type={"text"}
+            {...register("serverName", { required: true })}
+          />
+          {/* TODO: Add styling here */}
+          {errors.serverName?.type === "required" && "Server name is required"}
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor={"connectionInfo"}>Connection Info</label>
+          <input
+            className={styles.input}
+            id={"connectionInfo"}
+            type={"text"}
+            {...register("connectionInfo", { required: true })}
+          />
+          {/* TODO: Add styling here */}
+          {errors.connectionInfo?.type === "required" &&
+            "Connection info is required"}
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor={"description"}>Description</label>
+          <textarea
+            className={styles.textarea}
+            id={"description"}
+            {...register("description")}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor={"mode"}>Mode</label>
+          <select className={styles.input} id={"mode"} {...register("mode")}>
+            <option value={"pvp"}>PvP</option>
+            <option value={"pve"}>PvE</option>
+          </select>
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor={"mods"}>Mods</label>
+          <select className={styles.input} id={"mods"} {...register("mods")}>
+            {/* TODO: Populate */}
+          </select>
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor={"isPasswordProtected"}>Password Protected</label>
+          <input
+            className={styles.checkbox}
+            id={"isPasswordProtected"}
+            type={"checkbox"}
+            {...register("isPasswordProtected")}
+          />
+        </div>
+
+        <button id={styles.submit}>Submit</button>
       </form>
     </SWRConfig>
   );
