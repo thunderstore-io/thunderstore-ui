93a6723d2431316e5c140721dd252c0f0bcd1ca2
Oksamies <asko.soisalo@gmail.com>
2022-09-27 10:09:55 +0300

    Improve GetModMan button
    Remove return and brackets
    Introduce a fix for the noopener vuln
    
    refs TS-1052

--- apps/sertra/components/GetModMan.tsx
+++ apps/sertra/components/GetModMan.tsx
@@ -1,19 +1,18 @@
 import styles from "./GetModMan.module.css";
 
-export const GetModMan = () => {
-  return (
-    <button
-      type="button"
-      className={styles.getModManButton}
-      onClick={(e) => {
-        e.preventDefault();
-        window.open(
-          "https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager",
-          "_blank"
-        );
-      }}
-    >
-      Get mod manager
-    </button>
-  );
-};
+export const GetModMan = () => (
+  <button
+    type="button"
+    className={styles.button}
+    onClick={(e) => {
+      e.preventDefault();
+      window.open(
+        "https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager",
+        "_blank",
+        "noopener,noreferrer"
+      );
+    }}
+  >
+    Get mod manager
+  </button>
+);
