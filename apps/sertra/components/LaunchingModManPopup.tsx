686218ba64dc111db516a64d31e12f74ba43e389
Oksamies <asko.soisalo@gmail.com>
2022-09-27 14:14:05 +0300

    Correct LaunchingModManPopup stylings
    Add the usage of FontAwesomeIcon
    Correct stylings of launching modman popup modal
    
    refs TS-1052

--- apps/sertra/components/LaunchingModManPopup.tsx
+++ apps/sertra/components/LaunchingModManPopup.tsx
@@ -1,6 +1,7 @@
 import { GetModMan } from "./buttons/GetModMan";
 import styles from "./LaunchingModManPopup.module.css";
-import CloseWindowX from "/public/fa-svgs/x-solid.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 interface JoinServerProps {
   togglePopup: () => void;
@@ -12,25 +13,27 @@ export const LaunchingModManPopup: React.FC<JoinServerProps> = ({
   return (
     <div className={styles.background}>
       <section className={styles.popUp}>
-        <section className={styles.header}>
+        <div className={styles.header}>
           <h2>Launching Mod Manager...</h2>
           <button className={styles.closeWindow} onClick={togglePopup}>
-            <CloseWindowX />
+            <FontAwesomeIcon icon={faXmark} />
           </button>
-        </section>
-        <ol className={styles.instructions}>
-          <li>Launch Mod Manager</li>
-          <li>Sync mods for selected server</li>
-          <li>Copy the connection address</li>
-          <li>Launch the game through Mod Manager</li>
-          <li>Use the address in-game to join the server</li>
-        </ol>
-        <section className={styles.footer}>
+        </div>
+        <div className={styles.content}>
+          <ol className={styles.instructions}>
+            <li>Launch Mod Manager</li>
+            <li>Sync mods for selected server</li>
+            <li>Copy the connection address</li>
+            <li>Launch the game through Mod Manager</li>
+            <li>Use the address in-game to join the server</li>
+          </ol>
+        </div>
+        <div className={styles.footer}>
           <span className={styles.footerLeft}>
             Don&apos;t have Thunderstore Mod Manager yet?
           </span>
           {<GetModMan />}
-        </section>
+        </div>
       </section>
     </div>
   );
