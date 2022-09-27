fead28596ad7236720cf2a2d7387849703a7b15b
Oksamies <asko.soisalo@gmail.com>
2022-09-27 11:54:40 +0300

    Replace Join Server popup closing X with svg
    refs TS-1052

--- apps/sertra/components/LaunchingModManPopup.tsx
+++ apps/sertra/components/LaunchingModManPopup.tsx
@@ -1,5 +1,6 @@
 import { GetModMan } from "./buttons/GetModMan";
 import styles from "./LaunchingModManPopup.module.css";
+import CloseWindowX from "/public/fa-svgs/x-solid.svg";
 
 interface JoinServerProps {
   setClose: () => void;
@@ -14,7 +15,7 @@ export const LaunchingModManPopup: React.FC<JoinServerProps> = ({
         <section className={styles.header}>
           <h2>Launching Mod Manager...</h2>
           <button className={styles.closeWindow} onClick={setClose}>
-            X
+            <CloseWindowX />
           </button>
         </section>
         <ol className={styles.instructions}>
