58233b8c66af7c2e25e0b985ccb16e772c9c63a9
Oksamies <asko.soisalo@gmail.com>
2022-09-27 15:11:02 +0300

    Correct GetModMan css selector name
    refs TS-1052

--- apps/sertra/components/buttons/GetModMan.tsx
+++ apps/sertra/components/buttons/GetModMan.tsx
@@ -2,7 +2,7 @@ import styles from "./GetModMan.module.css";
 
 export const GetModMan = () => (
   <a
-    className={styles.button}
+    className={styles.buttonLink}
     href="https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager"
     target="_blank"
     rel="noopener noreferrer"
