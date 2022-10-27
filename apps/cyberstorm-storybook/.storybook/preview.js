171542b3a986f8fdba5452dded5dd19377c39ec3
Vili Manninen <vili.manninen@riskidev.fi>
2022-10-27 14:51:58 +0300

    change background color to surface 1

--- apps/cyberstorm-storybook/.storybook/preview.js
+++ apps/cyberstorm-storybook/.storybook/preview.js
@@ -5,7 +5,7 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   backgrounds: {
     default: "thunderstore",
-    values: [{ name: "thunderstore", value: "var(--color-surface--3)" }],
+    values: [{ name: "thunderstore", value: "var(--color-surface--1)" }],
   },
   controls: {
     matchers: {
