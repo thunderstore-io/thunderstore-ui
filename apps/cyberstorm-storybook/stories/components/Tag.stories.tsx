552779a6f9b4034ea8f04270a152f5213551408e
Vili Manninen <vili.manninen@riskidev.fi>
2022-11-22 12:33:23 +0200

    Refactor css, match the design

--- apps/cyberstorm-storybook/stories/components/Tag.stories.tsx
+++ apps/cyberstorm-storybook/stories/components/Tag.stories.tsx
@@ -2,7 +2,11 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Tag } from "@thunderstore/cyberstorm";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFlag, faFaceDizzy } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFlag,
+  faFaceDizzy,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 const meta = {
   title: "Cyberstorm/Components/Tag",
@@ -13,10 +17,29 @@ const defaultArgs = {
   label: "-",
 };
 
-const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
+const Template: ComponentStory<typeof Tag> = (args) => (
+  <div style={{ display: "flex", gap: "0.25em" }}>
+    <Tag {...args} />
+    <Tag {...args} />
+    <Tag {...args} />
+  </div>
+);
 
 const DefaultTag = Template.bind({});
 DefaultTag.args = { ...defaultArgs };
+
+const DesignTag = Template.bind({});
+DesignTag.args = {
+  ...defaultArgs,
+  label: "tag",
+  rightIcon: (
+    <FontAwesomeIcon
+      className="tagIcon tagIconRight"
+      fixedWidth={true}
+      icon={faXmark}
+    />
+  ),
+};
 
 const IconTag = Template.bind({});
 IconTag.args = {
@@ -38,4 +61,4 @@ IconTag.args = {
   ),
 };
 
-export { meta as default, DefaultTag, IconTag };
+export { meta as default, DefaultTag, DesignTag, IconTag };
