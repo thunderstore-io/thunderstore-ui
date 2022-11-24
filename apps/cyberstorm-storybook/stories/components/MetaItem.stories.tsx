22253b800b0a2d384e3d04c6ef00d8c151c2f868
Vili Manninen <104134747+VilppeRiskidev@users.noreply.github.com>
2022-11-24 13:19:35 +0200

    Cyberstorm UI: Add MetaItem component
    * Add MetaItem and refactor text sizes
    
    * Refactor MetaItem text sizes
    
    * Make Default MetaItem more default
    
    * Use css module correctly, refactor css sizes
    
    * Add defaultProp for MetaItem's style

---
+++ apps/cyberstorm-storybook/stories/components/MetaItem.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MetaItem } from "@thunderstore/cyberstorm";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faUser } from "@fortawesome/free-solid-svg-icons";
+
+const meta = {
+  title: "Cyberstorm/Components/MetaItem",
+  component: MetaItem,
+} as ComponentMeta<typeof MetaItem>;
+
+const defaultArgs = {
+  label: "-",
+  metaItemStyle: "metaItem__default",
+};
+
+const Template: ComponentStory<typeof MetaItem> = (args) => (
+  <MetaItem {...args} />
+);
+
+const DefaultMetaItem = Template.bind({});
+DefaultMetaItem.args = {
+  ...defaultArgs,
+};
+
+const GreenMetaItem = Template.bind({});
+GreenMetaItem.args = {
+  ...defaultArgs,
+  label: "Gigamies5000",
+  metaItemStyle: "metaItem__green",
+  icon: (
+    <FontAwesomeIcon
+      fixedWidth={true}
+      icon={faUser}
+      className={"metaItemIcon"}
+    />
+  ),
+};
+
+const LikesMetaItem = Template.bind({});
+LikesMetaItem.args = {
+  ...defaultArgs,
+  label: "1,342",
+  metaItemStyle: "metaItem__default",
+  icon: (
+    <FontAwesomeIcon
+      fixedWidth={true}
+      icon={faThumbsUp}
+      className={"metaItemIcon"}
+    />
+  ),
+};
+
+export { meta as default, DefaultMetaItem, GreenMetaItem, LikesMetaItem };
