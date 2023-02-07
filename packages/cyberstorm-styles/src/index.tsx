e5771b8b588f9a06c3edb353a5627466a4166701
Vili Manninen <vili.manninen@riskidev.fi>
2023-02-07 22:19:48 +0200

    Remove duplicate row

--- packages/cyberstorm-styles/src/index.tsx
+++ packages/cyberstorm-styles/src/index.tsx
@@ -1,6 +1,5 @@
 import "./styles/globals.css";
 import "./styles/colors.css";
 import "./styles/theme.css";
-import "./styles/globals.css";
 import "./styles/fonts.css";
 import "./styles/reset.css";
