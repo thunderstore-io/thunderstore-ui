09df858257123f7b13d01f338e9ab8e2b5100f40
Vili Manninen <vili.manninen@riskidev.fi>
2023-02-07 22:00:16 +0200

    Add separated css:s to package index

--- packages/cyberstorm-styles/src/index.tsx
+++ packages/cyberstorm-styles/src/index.tsx
@@ -1,3 +1,6 @@
 import "./styles/globals.css";
+import "./styles/colors.css";
+import "./styles/theme.css";
+import "./styles/globals.css";
 import "./styles/fonts.css";
 import "./styles/reset.css";
