908e244e769e61b7b03a3fa282edade040080b99
Vili Manninen <vili.manninen@riskidev.fi>
2023-03-22 22:10:35 +0200

    Map out some kind of data-schema

--- packages/cyberstorm/src/schema/schema.ts
+++ packages/cyberstorm/src/schema/schema.ts
@@ -1,19 +1,30 @@
 export interface CommunityData {
-  id: string; //url/link is based on this?
   name: string;
-  imageSource: string;
+  nameSpace: string;
+  imageSource?: string;
   packageCount: string;
   downloadCount: string;
   serverCount: string;
-  description: string;
-  githubLink?: string;
+  description?: string;
+  gitHubLink?: string;
 }
 
+type CommunityPreviewData = Pick<
+  CommunityData,
+  | "name"
+  | "nameSpace"
+  | "imageSource"
+  | "packageCount"
+  | "downloadCount"
+  | "serverCount"
+>;
+
 export interface PackageData {
-  id: string; //url/link is based on this?
-  packageName: string;
+  name: string;
+  nameSpace: string;
   description?: string;
-  imageSrc?: string;
+  imageSrc: string;
+  additionalImages?: string[];
   downloadCount: string;
   likes: string;
   size: string;
@@ -22,14 +33,129 @@ export interface PackageData {
   isPinned?: boolean;
   isNsfw?: boolean;
   isDeprecated?: boolean;
-  categories: string[];
+  categories?: string[];
+  gitHubLink?: string;
+  donationLink?: string;
+  firstUploaded?: string;
+  dependencyString?: string;
+  dependencies?: string[]; // Package ids
+  dependants?: string[]; // Package ids
+  team: string; // Team id
+}
+
+type PackagePreviewData = Pick<
+  PackageData,
+  | "name"
+  | "nameSpace"
+  | "description"
+  | "imageSrc"
+  | "downloadCount"
+  | "likes"
+  | "size"
+  | "author"
+  | "lastUpdated"
+  | "isPinned"
+  | "isNsfw"
+  | "isDeprecated"
+  | "categories"
+>;
+
+export interface BadgeData {
+  name: string;
+  nameSpace: string;
+  description: string;
+  imageSource: string;
+}
+
+export interface AchievementData {
+  name: string;
+  nameSpace: string;
+  description: string;
+  imageSource: string;
+}
+
+export interface TeamData {
+  name: string;
+  nameSpace: string;
+  description: string;
+  about: string;
+  members: string[]; // TeamMember ids
+  serviceAccounts: string[]; // ServiceAccount ids
+}
+
+export interface TeamMember {
+  user: string; // User id
+  role: string;
+}
+
+export interface UserData {
+  name: string;
+  nameSpace: string;
+  imageSource?: string;
+  description?: string;
+  about?: string;
+  gitHubLink?: string;
+  discordLink?: string;
+  twitterLink?: string;
+  accountCreated?: Date;
+  lastActive?: Date;
+  mods?: string[]; // Package ids
+}
+
+// TODO: figure out
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface UserSubscriptionData {}
+
+export interface HomePageViewData {
+  topCommunities: CommunityPreviewData[];
+  featuredMods: PackagePreviewData[];
+  firstTrendingMods: PackagePreviewData[];
+  secondTrendingMods: PackagePreviewData[];
 }
 
 export interface CommunitiesViewData {
-  communities: Array<CommunityData>; //only id, title, imageSource, packageCount, downloadCount, serverCount are needed
+  communities: CommunityPreviewData[];
 }
 
 export interface PackageListViewData {
   communityInfo: CommunityData;
-  packages: Array<PackageData>;
+  packages: PackagePreviewData[];
+}
+
+export interface PackageDetailViewData {
+  packageInfo: PackageData;
+  dependencies: PackagePreviewData[];
+}
+
+export interface UserProfileViewData {
+  userInfo: UserData;
+  userSubscriptionInfo: UserSubscriptionData;
+  teams: TeamData[];
+  badges?: BadgeData[];
+  achievements?: AchievementData[];
+}
+
+export interface UserSettingsViewData {
+  userInfo: UserData;
+  userSubscriptionInfo: UserSubscriptionData;
+  teams: TeamData[];
+  badges?: BadgeData[];
+  achievements?: AchievementData[];
+}
+
+export interface TeamSettingsViewData {
+  teamData: TeamData;
+}
+
+export interface PackageUploadViewData {
+  teams: TeamData[]; // Team ids
+  categories: string[];
+}
+
+export interface TermsOfServiceViewData {
+  termsOfService: string;
+  version: string;
+  link: string;
+  publishedDate: Date;
+  isLatest: boolean;
 }
