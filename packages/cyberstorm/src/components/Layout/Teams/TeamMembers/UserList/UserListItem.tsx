6263de502435893f5555149e9b03dce4a9ef50a4
Vili Manninen <104134747+VilppeRiskidev@users.noreply.github.com>
2023-04-17 13:40:47 +0300

    Fix some settings stuff (#653)

--- packages/cyberstorm/src/components/Layout/Teams/TeamMembers/UserList/UserListItem.tsx
+++ packages/cyberstorm/src/components/Layout/Teams/TeamMembers/UserList/UserListItem.tsx
@@ -7,8 +7,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export interface UserListItemProps {
-  userName?: string;
-  userImageSrc?: string;
+  userName: string;
+  userImageSrc: string;
   role?: string;
 }
 
