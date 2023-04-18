7b31f0372878e10fb15f24a52cd711ddbba1a263
Oksamies <asko.soisalo@gmail.com>
2023-04-18 12:17:10 +0300

    Revert Document back into non-class based fucntion

--- apps/cyberstorm-nextjs/pages/_document.tsx
+++ apps/cyberstorm-nextjs/pages/_document.tsx
@@ -1,15 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render(): JSX.Element {
-    return (
-      <Html lang="en">
-        <Head></Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
