9acb894ed076777ad9dd95dafc8b1c2e251da339
Cyanideankst <lauri.soisalo@teklity.com>
2023-05-22 17:00:55 +0300

    add MetaInfoItemList component

---
+++ packages/cyberstorm/src/components/MetaInfoItemList/MetaInfoItemList.tsx
@@ -0,0 +1,38 @@
+import styles from "../MetaInfoItemList/MetaInfoItemList.module.css";
+import React, { ReactElement } from "react";
+import { MetaInfoItem } from "../MetaInfoItem/MetaInfoItem";
+
+interface metaInfoData {
+  key: string;
+  label: string;
+  content: ReactElement;
+}
+export type MetaInfoItemListProps = {
+  metaInfoData?: Array<metaInfoData>;
+};
+
+/**
+ * Cyberstorm FilterItemList
+ */
+export const MetaInfoItemList: React.FC<MetaInfoItemListProps> = (props) => {
+  const { metaInfoData } = props;
+  const metaItemList = metaInfoData?.map((metaInfoDataItem) => {
+    return (
+      <div key={metaInfoDataItem.key}>
+        <MetaInfoItem
+          label={metaInfoDataItem.label}
+          content={metaInfoDataItem.content}
+        />
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className={styles.root}></div>
+      {metaItemList}
+    </div>
+  );
+};
+
+MetaInfoItemList.displayName = "MetaInfoItemList";
+MetaInfoItemList.defaultProps = { metaInfoData: [] };
