533dabe6910f5217d74a8f64571444c24fdfd252
Mythic <mythic@thunderstore.io>
2023-07-13 00:18:47 +0300

    CS: Remove unnecessary style import

--- apps/cyberstorm-nextjs/app/page.tsx
+++ apps/cyberstorm-nextjs/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { HomeLayout } from "@thunderstore/cyberstorm";
-import "@thunderstore/cyberstorm-styles";
 
 export default function RootLayout() {
   return <HomeLayout />;
