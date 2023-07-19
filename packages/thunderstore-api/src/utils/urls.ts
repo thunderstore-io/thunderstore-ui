b455edd28ce4d5931c7fe767ab142b18e65382a7
Mythic <mythic@thunderstore.io>
2023-07-19 06:31:55 +0300

    thunderstore-api: Improve URL builder

--- packages/thunderstore-api/src/utils/urls.ts
+++ packages/thunderstore-api/src/utils/urls.ts
@@ -1,44 +1,55 @@
-type Resolver = {
+type State = {
   _path: string;
-  resolve: () => string;
 };
 
-export function R(path: string): Resolver;
+type Resolver<Resolution> = {
+  resolve: () => Resolution;
+};
+
+export function R(path: string): Resolver<string> & State;
 export function R<Params>(
   path: (params: Params) => string
-): (params: Params) => Resolver;
+): (params: Params) => Resolver<string> & State;
 export function R<Children>(
   path: string,
   children: Children
-): Children & Resolver;
+): Children & Resolver<string> & State;
 export function R<Params, Children>(
   path: (params: Params) => string,
   children: Children
-): (params: Params) => Children & Resolver;
-export function R<Params, Children extends {}>(
+): (params: Params) => Children & Resolver<string> & State;
+export function R<Resolution>(
+  path: string,
+  children: undefined,
+  resolver: (path: string) => Resolution
+): Resolver<Resolution> & State;
+export function R<Params, Children extends {}, Resolution>(
   path: string | ((params: Params) => string),
-  children?: Children & Resolver
+  children?: Children & State,
+  resolver?: (path: string) => Resolution
 ):
-  | ((Children & Resolver) | Resolver)
-  | ((params: Params) => (Children & Resolver) | Resolver) {
+  | (
+      | (Children & Resolver<Resolution | string> & State)
+      | (Resolver<Resolution | string> & State)
+    )
+  | ((
+      params: Params
+    ) =>
+      | (Children & Resolver<Resolution | string> & State)
+      | (Resolver<Resolution | string> & State)) {
   if (typeof path !== "string") {
     return (params: Params) => {
       const resolved = path(params);
       return R(resolved, children);
     };
   }
-  const _resolved = (children?._path ?? "") + path;
 
-  let result: (Children & Resolver) | Resolver;
-  if (children) {
-    children.resolve = () => _resolved;
-    children._path = _resolved;
-    result = children;
-  } else {
-    result = {
-      _path: _resolved,
-      resolve: () => _resolved,
-    };
-  }
-  return result;
+  const _fullPath = (children?._path ?? "") + path;
+  const _resolve = resolver ? () => resolver(_fullPath) : () => _fullPath;
+
+  return {
+    ...children,
+    _path: _fullPath,
+    resolve: _resolve,
+  };
 }
