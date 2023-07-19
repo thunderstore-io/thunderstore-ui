665e99e330d15692ab1f390165db39ae5eca3e6b
Mythic <mythic@thunderstore.io>
2023-07-19 06:47:16 +0300

    thunderstore-api: Improve router type hints

--- packages/thunderstore-api/src/utils/urls.ts
+++ packages/thunderstore-api/src/utils/urls.ts
@@ -1,42 +1,37 @@
-type State = {
-  _path: string;
-};
-
 type Resolver<Resolution> = {
+  _path: string;
   resolve: () => Resolution;
 };
 
-export function R(path: string): Resolver<string> & State;
+type RouteChildren<Children, Resolution> =
+  | (Resolver<Resolution> & Children)
+  | Resolver<Resolution>;
+type RouteLeaf<Params, Children, Resolution> =
+  | RouteChildren<Children, Resolution | string>
+  | ((params: Params) => RouteChildren<Children, Resolution | string>);
+
+export function R(path: string): Resolver<string>;
 export function R<Params>(
   path: (params: Params) => string
-): (params: Params) => Resolver<string> & State;
+): (params: Params) => Resolver<string>;
 export function R<Children>(
   path: string,
   children: Children
-): Children & Resolver<string> & State;
+): Children & Resolver<string>;
 export function R<Params, Children>(
   path: (params: Params) => string,
   children: Children
-): (params: Params) => Children & Resolver<string> & State;
+): (params: Params) => Children & Resolver<string>;
 export function R<Resolution>(
   path: string,
   children: undefined,
   resolver: (path: string) => Resolution
-): Resolver<Resolution> & State;
+): Resolver<Resolution>;
 export function R<Params, Children extends {}, Resolution>(
   path: string | ((params: Params) => string),
-  children?: Children & State,
+  children?: RouteChildren<Children, unknown>,
   resolver?: (path: string) => Resolution
-):
-  | (
-      | (Children & Resolver<Resolution | string> & State)
-      | (Resolver<Resolution | string> & State)
-    )
-  | ((
-      params: Params
-    ) =>
-      | (Children & Resolver<Resolution | string> & State)
-      | (Resolver<Resolution | string> & State)) {
+): RouteLeaf<Params, Children, Resolution> {
   if (typeof path !== "string") {
     return (params: Params) => {
       const resolved = path(params);
