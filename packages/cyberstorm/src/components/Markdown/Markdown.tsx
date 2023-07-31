5f895c0182e92025745bcca2839170ffc804e42e
Antti Mäki <antti@anttimaki.com>
2023-07-31 17:23:44 +0300

    @thunderstore/cyberstorm: fix Markdown component styling
    The implementation copy-pasted from the old source did nothing due to
    different tooling between the projects, and we want to CSS modules here
    anyway.
    
    The styles are only placeholders, since finding the actual styles for
    all use cases might require some consultation from our resident
    designer.
    
    Refs TS-1731

--- packages/cyberstorm/src/components/Markdown/Markdown.tsx
+++ packages/cyberstorm/src/components/Markdown/Markdown.tsx
@@ -2,6 +2,8 @@
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 
+import styles from "./Markdown.module.css";
+
 interface MarkdownProps {
   children: string;
 }
@@ -13,45 +15,10 @@ export function Markdown(props: MarkdownProps) {
   const { children } = props;
 
   return (
-    <ReactMarkdown className="markdown" plugins={[gfm]}>
+    <ReactMarkdown className={styles.root} plugins={[gfm]}>
       {children}
     </ReactMarkdown>
   );
 }
-
-export const markdownStyles = {
-  ".markdown": {
-    color: "ts.white",
-    fontFamily: "Exo2, sans-serif",
-
-    "h1, h2": {
-      paddingBottom: "0.25em",
-      borderBottom: "1px solid #c4c4c4",
-      marginBottom: "0.5em",
-    },
-    h1: { fontSize: "32px" },
-    h2: { fontSize: "24px" },
-    h3: { fontSize: "18px" },
-    h4: { fontSize: "16px" },
-    h5: { fontSize: "14px" },
-    h6: { fontSize: "12px" },
-    p: {
-      fontSize: "16px",
-      fontWeight: "500",
-      lineHeight: "1.2",
-    },
-    "img, ul, ol, p": { marginBottom: "1.25em" },
-    img: { display: "inline-block" },
-    "li > ul, li > ol": {
-      marginTop: "0.25em",
-    },
-    code: {
-      color: "ts.blue",
-      padding: "3px 6px",
-    },
-    "pre > code": { padding: "1em" },
-    a: { color: "ts.babyBlue" },
-  },
-};
 
 Markdown.displayName = "Header";
