4b09a74faba5267cbe62aa19f7b269342e3996b8
Vili Manninen <vili.manninen@riskidev.fi>
2023-08-16 16:36:18 +0300

    Polish error page

--- apps/cyberstorm-nextjs/app/error.tsx
+++ apps/cyberstorm-nextjs/app/error.tsx
@@ -1,5 +1,6 @@
+"use client";
 import { ErrorLayout } from "@thunderstore/cyberstorm";
 
-export default function Error({}: { error: Error; reset: () => void }) {
+export default function Error() {
   return <ErrorLayout error={500} />;
 }
