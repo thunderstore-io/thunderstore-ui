7cea7ed55d173277f21a0d519e7b4a073a34af79
Antti Mäki <antti@anttimaki.com>
2023-08-22 17:52:55 +0300

    @thunderstore/thunderstore-api: add naive implementation
    This proof-of-concept gives an idea what is the intention of the
    changes. The changes themselves will be implemented separately to
    prevent the PR from getting too big.
    
    Refs TS-1799

--- packages/thunderstore-api/src/__tests__/index.ts
+++ packages/thunderstore-api/src/__tests__/index.ts
@@ -1,4 +1,13 @@
-// Placeholder test while waiting for implementation.
-it("Checks that tests are run without problems", () => {
-  expect(true).toStrictEqual(true);
+import { fetchCommunityList } from "../index";
+
+it("finds RoR2 in community listing", async () => {
+  const response = await fetchCommunityList();
+
+  expect(Array.isArray(response.results)).toEqual(true);
+
+  const ror = response.results.find(
+    (x: { identifier: string }) => x.identifier === "riskofrain2"
+  );
+
+  expect(ror).toBeDefined();
 });
