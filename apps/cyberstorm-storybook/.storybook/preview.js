795e80fb1944fb4842d827ee4bfc4fc086224ee3
Antti Mäki <antti@anttimaki.com>
2023-08-25 17:11:12 +0300

    @thunderstore/cyberstorm-storybook: switch to using DapperFake
    The old DummyDapper will be phased out as a part of refactoring aiming
    to improve the structure and understandability of the Dapper as a
    whole.
    
    Refs TS-1805

--- apps/cyberstorm-storybook/.storybook/preview.js
+++ apps/cyberstorm-storybook/.storybook/preview.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { LinkLibrary } from "../LinkLibrary";
+
 import { LinkingProvider, CyberstormProviders } from "@thunderstore/cyberstorm";
 import "@thunderstore/cyberstorm-styles";
-import { DapperProvider, DummyDapper } from "@thunderstore/dapper";
+import { DapperProvider } from "@thunderstore/dapper";
+import { DapperFake } from "@thunderstore/dapper-fake";
+import { LinkLibrary } from "../LinkLibrary";
 import { SessionProvider } from "../SessionContext";
 
 export const parameters = {
@@ -26,21 +28,14 @@ export const decorators = [
       <LinkingProvider value={LinkLibrary}>
         <CyberstormProviders>
           <SessionProvider>
-            <Substack>
-              <Story />
-            </Substack>
+            <DapperProvider dapperConstructor={() => new DapperFake()}>
+              <LinkingProvider value={LinkLibrary}>
+                <Story />
+              </LinkingProvider>
+            </DapperProvider>
           </SessionProvider>
         </CyberstormProviders>
       </LinkingProvider>
     );
   },
 ];
-
-function Substack({ children }) {
-  const dapperConstructor = () => new DummyDapper();
-  return (
-    <DapperProvider dapperConstructor={dapperConstructor}>
-      <LinkingProvider value={LinkLibrary}>{children}</LinkingProvider>
-    </DapperProvider>
-  );
-}
