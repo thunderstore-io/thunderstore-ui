2014028e7212007387b57e4a4483a9f377b7fb97
Antti Mäki <antti@anttimaki.com>
2023-08-31 13:05:36 +0300

    @thunderstore/dapper: remove dummy data generation
    Since the main Dapper package focuses on providing the interface, it no
    longer needs to provide dummy data. There's dapper-fake for that.
    
    Refs TS-1811

--- packages/dapper/src/index.ts
+++ packages/dapper/src/index.ts
@@ -1,6 +1,3 @@
 export { DapperProvider, useDapper } from "./context";
 export type { DapperInterface } from "./dapper";
 export { DapperError } from "./errors";
-
-// Temporary exports while Dapper refactoring is going on.
-export * from "./implementations/dummy/generate";
