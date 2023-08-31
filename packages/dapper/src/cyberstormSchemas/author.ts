2e7c5d530e340663d4bd78648e98f2b530e9248d
Antti Mäki <antti@anttimaki.com>
2023-08-31 13:14:39 +0300

    @thunderstore/dapper: remove data format validation
    Since the main Dapper package focuses on providing the interface, it no
    longer needs to validate the data received from external sources.
    
    This will probably pop up in @thunderstore/thunderstore-api in the
    future.
    
    Refs TS-1811

--- packages/dapper/src/cyberstormSchemas/author.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const authorSchema = z.object({
-  user: z.string().min(1), // TODO: Probably needs to be updated to User schema, but after the auth is done.
-  imageSource: z.optional(z.string()),
-  role: z.string(),
-});
--- packages/dapper/src/cyberstormSchemas/connection.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const connectionSchema = z.object({
-  name: z.string(),
-  connectedUsername: z.string(),
-  imageSource: z.string(),
-  enabled: z.boolean(),
-});
