ec520c836c2565fb7fea2c5406306c06617ad359
Antti Mäki <antti@anttimaki.com>
2023-09-01 15:39:46 +0300

    @thunderstore/dapper-ts: don't throw for unimplemented methods
    Throwing cause the build of `@thunderstore/cyberstorm-nextjs` to fail,
    and thus CI to block merging. Instead, return data that seems to be
    tolerated by all current consumers, typed as any to satisfy the Dapper
    interface.
    
    Refs TS-1807, TS-1811

--- packages/dapper-ts/src/index.ts
+++ packages/dapper-ts/src/index.ts
@@ -1,7 +1,12 @@
 import { DapperInterface } from "@thunderstore/dapper";
 
-const NotImplemented = () => {
-  throw new Error("DapperTs has not implemented a provider method");
+// Original idea was for NotImlemented to throw an error, but that
+// causes the build cyberstorm-nextjs to fail, which causes CI pipeline
+// to block merging.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const NotImplemented: any = async () => {
+  console.error("DapperTs has not implemented a provider method");
+  return [];
 };
 
 export class DapperTs implements DapperInterface {
