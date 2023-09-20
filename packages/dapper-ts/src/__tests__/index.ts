709cab9088318838f538d906356867e7d9c12052
Antti Mäki <antti@anttimaki.com>
2023-09-20 16:49:54 +0300

    @thunderstore/dapper-ts: add unit tests
    Tests should check that each method from DapperInterface can be
    executed without errors.
    
    This does duplicate work (sending requests to test environment) with
    @thunderstore/thunderstore-api. It might make sense to drop the
    endpoint specific tests from that package.
    
    Refs TS-1815

---
+++ packages/dapper-ts/src/__tests__/index.ts
@@ -0,0 +1,51 @@
+import { DapperTs } from "../index";
+
+let dapper: DapperTs;
+
+beforeAll(() => {
+  dapper = new DapperTs({ apiHost: "https://thunderstore.dev" });
+});
+
+it("executes getCommunities without errors", async () => {
+  await expect(dapper.getCommunities()).resolves.not.toThrowError();
+});
+
+it("executes getCommunity without errors", async () => {
+  await expect(dapper.getCommunity()).resolves.not.toThrowError();
+});
+
+it("executes getPackage without errors", async () => {
+  await expect(dapper.getPackage()).resolves.not.toThrowError();
+});
+
+it("executes getPackageDependencies without errors", async () => {
+  await expect(dapper.getPackageDependencies()).resolves.not.toThrowError();
+});
+
+it("executes getPackageListings without errors", async () => {
+  await expect(dapper.getPackageListings()).resolves.not.toThrowError();
+});
+
+it("executes getServiceAccount without errors", async () => {
+  await expect(dapper.getServiceAccount()).resolves.not.toThrowError();
+});
+
+it("executes getServiceAccountList without errors", async () => {
+  await expect(dapper.getServiceAccountList()).resolves.not.toThrowError();
+});
+
+it("executes getTeam without errors", async () => {
+  await expect(dapper.getTeam()).resolves.not.toThrowError();
+});
+
+it("executes getTeamList without errors", async () => {
+  await expect(dapper.getTeamList()).resolves.not.toThrowError();
+});
+
+it("executes getUser without errors", async () => {
+  await expect(dapper.getUser()).resolves.not.toThrowError();
+});
+
+it("executes getUserSettings without errors", async () => {
+  await expect(dapper.getUserSettings()).resolves.not.toThrowError();
+});
