751c2d545c6c1a09aa62447938f9ba3cf9c420b3
Antti Mäki <antti@anttimaki.com>
2023-09-20 16:21:36 +0300

    @thunderstore/dapper-ts: implement getCommunities
    Pass the arguments to corresponding thunderstore-api method and use Zod
    to validate the received data.
    
    It was earlier suspected that Zod caused the dev server to run out of
    memory, but based on some testing this no longer seems to be the case.
    This might be due to upgraded Zod version, or maybe the problem doesn't
    show itself with just one validator in place. Time will tell.
    
    Refs TS-1815

--- packages/dapper-ts/src/methods/communities.ts
+++ packages/dapper-ts/src/methods/communities.ts
@@ -1,16 +1,40 @@
-import { DapperTsInterface } from "../index";
+import { z } from "zod";
+import { fetchCommunityList } from "@thunderstore/thunderstore-api";
 
-// TODO: this is a dummy implementation required to prevent cyberstorm
-// build from breaking.
+import { DapperTsInterface } from "../index";
+import { paginatedResults } from "../sharedSchemas";
+
+const communitySchema = z.object({
+  name: z.string().nonempty(),
+  identifier: z.string().nonempty(),
+  description: z.string().nullable(),
+  discord_url: z.string().nullable(),
+  datetime_created: z.string().datetime(),
+  background_image_url: z.string().url().nullable(),
+  icon_url: z.string().url().nullable(),
+  total_download_count: z.number().int(),
+  total_package_count: z.number().int(),
+});
+
+const schema = paginatedResults(communitySchema);
+
 export async function getCommunities(
   this: DapperTsInterface,
   page = 1,
   ordering = "name",
   search?: string
 ) {
+  const data = await fetchCommunityList(this.config, page, ordering, search);
+  const parsed = schema.safeParse(data);
+
+  if (!parsed.success) {
+    // TODO: add Sentry support and log parsed.error.
+    throw new Error("Invalid data received from backend");
+  }
+
   return {
-    count: page * 150,
-    hasMore: ordering === search,
-    results: [],
+    count: parsed.data.count,
+    hasMore: Boolean(parsed.data.next),
+    results: parsed.data.results,
   };
 }
