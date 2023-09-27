2a41b3ba80e064b85eeff787af374f21e53adb9e
Antti Mäki <antti@anttimaki.com>
2023-09-27 17:01:30 +0300

    @thunderstore/dapper-fake: clean up obsolete legacy support

--- packages/dapper-fake/src/index.ts
+++ packages/dapper-fake/src/index.ts
@@ -13,10 +13,6 @@ import {
 import { getFakeTeam, getFakeTeams } from "./fakers/team";
 import { getFakeUser, getFakeUserSettings } from "./fakers/user";
 
-const NotImplemented = () => {
-  throw new Error("DapperFake has not implemented a provider method");
-};
-
 export class DapperFake implements DapperInterface {
   public getCommunities = getFakeCommunities;
   public getCommunity = getFakeCommunity;
@@ -29,11 +25,4 @@ export class DapperFake implements DapperInterface {
   public getTeamList = getFakeTeams;
   public getUser = getFakeUser;
   public getUserSettings = getFakeUserSettings;
-
-  // Legacy for backwards compatibility. Can be removed once support for
-  // @thunderstore/nextjs app and @thunderstore/components package are
-  // dropped.
-  public getFrontpage = NotImplemented;
-  public getOldPackage = NotImplemented;
-  public getOldCommunityPackageListing = NotImplemented;
 }
