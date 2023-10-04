cce36379365454f5488f761027bb26a7db984980
Oksamies <asko.soisalo@gmail.com>
2023-10-04 15:33:58 +0300

    markdown styles and validation
    Add missing styles for markdown component
    Add validation to the markdown preview
    
    refs TS-1731

--- packages/cyberstorm/src/components/Layout/Developers/MarkdownPreview/MarkdownPreviewLayout.tsx
+++ packages/cyberstorm/src/components/Layout/Developers/MarkdownPreview/MarkdownPreviewLayout.tsx
@@ -4,16 +4,89 @@ import { BreadCrumbs } from "../../../BreadCrumbs/BreadCrumbs";
 import { MarkdownPreviewLink } from "../../../Links/Links";
 import { BaseLayout } from "../../BaseLayout/BaseLayout";
 import { PageHeader } from "../../BaseLayout/PageHeader/PageHeader";
-import { useState } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { Markdown } from "../../../Markdown/Markdown";
 import styles from "./MarkdownPreviewLayout.module.css";
 import { TextAreaInput } from "../../../TextAreaInput/TextAreaInput";
+import { Icon } from "../../../Icon/Icon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faTriangleExclamation,
+  faPenToSquare,
+  faArrowsRotate,
+} from "@fortawesome/free-solid-svg-icons";
 
 /**
  * Cyberstorm MarkdownPreview Layout
  */
 export function MarkdownPreviewLayout() {
   const [markdownPreviewInput, setMarkdownPreviewInput] = useState("");
+  const [validationStatus, setValidationStatus] = useState<
+    "waiting" | "validating" | "success" | "failure"
+  >("waiting");
+
+  let statusElement = null;
+  if (validationStatus === "waiting") {
+    statusElement = (
+      <div className={styles.statusBar}>
+        <div className={styles.icon}>
+          <Icon>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </Icon>
+        </div>
+        Waiting for input
+      </div>
+    );
+  } else if (validationStatus === "validating") {
+    statusElement = (
+      <div className={styles.statusBar}>
+        <div className={styles.icon}>
+          <Icon>
+            <FontAwesomeIcon icon={faArrowsRotate} />
+          </Icon>
+        </div>
+        Validating...
+      </div>
+    );
+  } else if (validationStatus === "success") {
+    statusElement = (
+      <div className={`${styles.statusBar} ${styles.statusBarSuccess}`}>
+        <div className={styles.icon}>
+          <Icon>
+            <FontAwesomeIcon icon={faCircleCheck} />
+          </Icon>
+        </div>
+        All systems go!
+      </div>
+    );
+  } else if (validationStatus === "failure") {
+    statusElement = (
+      <div className={`${styles.statusBar} ${styles.statusBarFailure}`}>
+        <div className={`${styles.icon} ${styles.iconFailure}`}>
+          <Icon>
+            <FontAwesomeIcon icon={faTriangleExclamation} />
+          </Icon>
+        </div>
+        Problem, alarm, danger. Everything is going to explode.
+      </div>
+    );
+  }
+
+  const input = (
+    <div
+      className={`${styles.inputContainer} ${
+        validationStatus === "failure" ? styles.inputContainerFailure : null
+      }`}
+    >
+      <TextAreaInput
+        placeHolder="# This is a markdown preview placeholder"
+        setValue={setMarkdownPreviewInput}
+        value={markdownPreviewInput}
+      />
+      {statusElement}
+    </div>
+  );
 
   return (
     <BaseLayout
@@ -28,18 +101,21 @@ export function MarkdownPreviewLayout() {
           <SettingItem
             title="Markdown input"
             description="Input your markdown code"
-            content={
-              <TextAreaInput
-                placeHolder="# This is a markdown preview placeholder"
-                setValue={setMarkdownPreviewInput}
-                value={markdownPreviewInput}
-              />
-            }
+            content={input}
           />
           <SettingItem
             title="Markdown output"
             description="A preview of your rendered markdown"
-            content={<Markdown>{markdownPreviewInput}</Markdown>}
+            content={
+              <Suspense
+                fallback={<h2 className={styles.showing}>🌀 Loading...</h2>}
+              >
+                <Markdown
+                  input={markdownPreviewInput}
+                  setStatus={setValidationStatus}
+                />
+              </Suspense>
+            }
           />
         </div>
       }
--- packages/cyberstorm/src/components/Markdown/Markdown.tsx
+++ packages/cyberstorm/src/components/Markdown/Markdown.tsx
@@ -1,22 +1,74 @@
 "use client";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 import styles from "./Markdown.module.css";
+import PLACEHOLDER from "./MarkdownPlaceholder";
+import { useEffect, useState, SetStateAction } from "react";
 
 interface MarkdownProps {
-  children: string;
+  input: string;
+  setStatus: React.Dispatch<
+    SetStateAction<"waiting" | "validating" | "success" | "failure">
+  >;
+}
+
+interface HTMLResponse {
+  html: string;
+}
+
+export const isRecord = (obj: unknown): obj is Record<string, unknown> =>
+  obj instanceof Object;
+
+function isHTMLResponse(response: unknown): response is HTMLResponse {
+  return isRecord(response) && typeof response.html === "string";
 }
 
 /**
  * Wrapper for rendering markdown strings as proper markup.
  */
 export function Markdown(props: MarkdownProps) {
-  const { children } = props;
+  const { input, setStatus } = props;
+  const placeholder = PLACEHOLDER();
+
+  const [html, setHTML] = useState<null | string>(null);
+  useEffect(() => {
+    async function getHTML() {
+      setStatus("validating");
+      const response = await fetch(
+        "http://thunderstore.temp/api/experimental/frontend/render-markdown/",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ markdown: input }),
+        }
+      );
+      const data = await response.json();
+      if (isHTMLResponse(data)) {
+        setHTML(data.html);
+        setStatus("success");
+      } else {
+        setStatus("failure");
+      }
+    }
+    if (input) {
+      getHTML();
+    } else {
+      setHTML(null);
+    }
+  }, [input]);
 
   return (
-    <ReactMarkdown className={styles.root} remarkPlugins={[gfm]}>
-      {children}
+    <ReactMarkdown
+      className={styles.root}
+      remarkPlugins={[gfm]}
+      rehypePlugins={[rehypeRaw]}
+    >
+      {html ? html : placeholder}
     </ReactMarkdown>
   );
 }
