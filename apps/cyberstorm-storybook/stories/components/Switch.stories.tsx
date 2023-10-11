e55843789e83615354aa224d2e8cdf54e00032d0
Cyanideankst <95920347+Cyanideankst@users.noreply.github.com>
2023-10-12 02:44:13 +0300

    Switch component touchable label element (#842)
    * add support for labels for the switch component
    add eslint rule to support custom components
    
    * fix storybook template naming
    
    * fix naming and clean unnecesary divs
    
    * fix controlComponent values
    
    * fix capitalization and delete unnecesary tags

--- apps/cyberstorm-storybook/stories/components/Switch.stories.tsx
+++ apps/cyberstorm-storybook/stories/components/Switch.stories.tsx
@@ -22,10 +22,37 @@ const SameStateSwitchTemplate: StoryFn<typeof Switch> = () => {
     </div>
   );
 };
+const SwitchLabelIdTemplate: StoryFn<typeof Switch> = () => {
+  const [state, setState] = useState(false);
+  return (
+    <>
+      <label htmlFor="my-switch">My switch</label>
+      <Switch state={state} onChange={setState} id="my-switch" />
+    </>
+  );
+};
+const SwitchNestedLabelTemplate: StoryFn<typeof Switch> = () => {
+  const [state, setState] = useState(false);
+  return (
+    <label>
+      My switch
+      <Switch state={state} onChange={setState} />
+    </label>
+  );
+};
 
 const RegularSwitch = Template.bind({});
 const DisabledSwitch = Template.bind({});
 DisabledSwitch.args = { state: false, disabled: true };
 const SameStateSwitch = SameStateSwitchTemplate.bind({});
+const SwitchNestedLabel = SwitchNestedLabelTemplate.bind({});
+const SwitchLabelId = SwitchLabelIdTemplate.bind({});
 
-export { meta as default, RegularSwitch, DisabledSwitch, SameStateSwitch };
+export {
+  meta as default,
+  RegularSwitch,
+  DisabledSwitch,
+  SameStateSwitch,
+  SwitchLabelId,
+  SwitchNestedLabel,
+};
--- packages/cyberstorm/src/components/Switch/Switch.tsx
+++ packages/cyberstorm/src/components/Switch/Switch.tsx
@@ -8,16 +8,18 @@ export interface SwitchProps {
   state: boolean;
   onChange?: Dispatch<SetStateAction<boolean>>;
   disabled?: boolean;
+  id?: string;
 }
 
 export function Switch(props: SwitchProps) {
-  const { state, onChange, disabled = false } = props;
+  const { state, onChange, disabled = false, id } = props;
   return (
     <RadixSwitch.Root
       className={styles.root}
       disabled={disabled}
       onCheckedChange={onChange}
       checked={state}
+      id={id}
     >
       <RadixSwitch.Thumb className={styles.thumb} />
     </RadixSwitch.Root>
