e0dfaeaf81e8d9a2c3c06d460156fc9a24a5f7c1
Mythic <mythic@thunderstore.io>
2023-11-10 07:53:34 +0200

    ts-uploader-react: Cache upload progress
    useSyncExternalStore requires caching of the external value to avoid
    re-rendering when no changes have occurred to it. That being the case,
    we can use a simpler combination of useState and useEffect to achieve
    the same result as useSyncExternalStore would have but with caching
    implicitly built in.

--- packages/ts-uploader-react/src/index.tsx
+++ packages/ts-uploader-react/src/index.tsx
@@ -1,23 +1,19 @@
 import { IUploadHandle, UploadProgress } from "@thunderstore/ts-uploader";
-import { useSyncExternalStore } from "react";
+import { useEffect, useState } from "react";
 
 export const useUploadProgress = (
   handle?: IUploadHandle
 ): UploadProgress | undefined => {
-  return useSyncExternalStore(
-    (onStoreChange) => {
-      if (handle) {
-        return handle.onProgress.addListener(() => onStoreChange());
-      } else {
-        return () => {};
-      }
-    },
-    () => {
-      if (handle) {
-        return handle.progress;
-      } else {
-        return undefined;
-      }
+  const [progress, setProgress] = useState<UploadProgress>();
+  useEffect(() => {
+    if (!handle) {
+      setProgress(undefined);
+      return () => {};
+    } else {
+      setProgress(handle.progress);
+      return handle.onProgress.addListener(setProgress);
     }
-  );
+  }, [handle]);
+
+  return progress;
 };
