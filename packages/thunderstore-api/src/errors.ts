362c3a96548bc06845a8813b3e9c387a801066a5
Mythic <mythic@thunderstore.io>
2023-11-23 17:30:24 +0200

    Improve API error parsing

--- packages/thunderstore-api/src/errors.ts
+++ packages/thunderstore-api/src/errors.ts
@@ -1,24 +1,61 @@
+type JSONValue =
+  | string
+  | number
+  | boolean
+  | { [x: string]: JSONValue }
+  | JSONValue[];
+
 export function isApiError(e: Error | ApiError | unknown): e is ApiError {
   return e instanceof ApiError;
 }
 
 export class ApiError extends Error {
-  response: { [key: string]: string[] } | undefined;
+  response: Response;
+  responseJson?: JSONValue;
 
-  constructor(message?: string, response?: { [key: string]: string[] }) {
-    super(message);
-    this.response = response;
+  constructor(args: {
+    message: string;
+    response: Response;
+    responseJson?: JSONValue;
+  }) {
+    super(args.message);
+    this.responseJson = args.responseJson;
+    this.response = args.response;
   }
 
   static async createFromResponse(response: Response): Promise<ApiError> {
-    // TODO: Implement response parsing for known error scenarios
-    return new ApiError(
-      `${response.status}: ${response.statusText}`,
-      await response.json()
-    );
+    let responseJson: JSONValue | undefined;
+    try {
+      responseJson = await response.json();
+    } catch (e) {
+      responseJson = undefined;
+    }
+
+    return new ApiError({
+      message: `${response.status}: ${response.statusText}`,
+      response: response,
+      responseJson: responseJson,
+    });
   }
 
-  getFieldErrors(): { [key: string]: string[] } {
-    return { ...this.response };
+  getFieldErrors(): { [key: string | "root"]: string[] } {
+    if (typeof this.responseJson !== "object")
+      return { root: ["Unknown error occurred"] };
+    if (Array.isArray(this.responseJson)) {
+      return {
+        root: this.responseJson.map((x) => x.toString()),
+      };
+    } else {
+      return Object.fromEntries(
+        Object.entries(this.responseJson).map(([key, val]) => {
+          return [
+            key,
+            Array.isArray(val)
+              ? val.map((x) => x.toString())
+              : [val.toString()],
+          ];
+        })
+      );
+    }
   }
 }
