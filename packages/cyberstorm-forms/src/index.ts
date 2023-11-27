83b4a30c0648e521e44253d70d33db222ff76708
Oksamies <34424527+Oksamies@users.noreply.github.com>
2023-11-27 17:29:18 +0200

    Add form component for MultiSelectSearch (#931)
    * Add FormMultiSelectSearch
    
    * Fix MultiSelectSearch onChange

--- packages/cyberstorm-forms/src/index.ts
+++ packages/cyberstorm-forms/src/index.ts
@@ -1,5 +1,6 @@
 export { useFormToaster } from "./useFormToaster";
 export { FormSubmitButton } from "./components/FormSubmitButton";
 export { FormSelectSearch } from "./components/FormSelectSearch";
+export { FormMultiSelectSearch } from "./components/FormMultiSelectSearch";
 export { FormTextInput } from "./components/FormTextInput";
 export { CreateTeamForm } from "./forms/CreateTeamForm";
