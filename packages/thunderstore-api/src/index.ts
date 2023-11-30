ded317eea25a444ac4c9fe5d49bcdabca83d0884
Oksamies <asko.soisalo@gmail.com>
2023-11-30 22:48:21 +0200

    Add AddTeamMemberForm

--- packages/thunderstore-api/src/index.ts
+++ packages/thunderstore-api/src/index.ts
@@ -21,4 +21,5 @@ export * from "./fetch/teamDetails";
 export * from "./fetch/teamMembers";
 export * from "./fetch/teamServiceAccounts";
 export * from "./fetch/teamCreate";
+export * from "./fetch/teamAddMember";
 export * from "./errors";
