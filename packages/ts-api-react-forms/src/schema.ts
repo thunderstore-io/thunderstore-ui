b08469879025accc101c4b8a724fb5489e7e56be
Oksamies <asko.soisalo@gmail.com>
2023-11-30 22:48:21 +0200

    Add AddTeamMemberForm

--- packages/ts-api-react-forms/src/schema.ts
+++ packages/ts-api-react-forms/src/schema.ts
@@ -5,3 +5,12 @@ export const createTeamFormSchema = z.object({
     .string({ required_error: "Team name is required" })
     .min(1, { message: "Team name is required" }),
 });
+
+export const teamAddMemberFormSchema = z.object({
+  user: z
+    .string({ required_error: "Username is required" })
+    .min(1, { message: "Username is required" }),
+  role: z
+    .string({ required_error: "Role is required" })
+    .min(1, { message: "Role is required" }),
+});
