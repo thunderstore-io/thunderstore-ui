bb7e6aa1c67578ccc34779d45b88c62543742b89
cyanideankst <lauri.soisalo@gmail.com>
2023-12-11 03:10:18 +0200

    fix flexing and border-radius

--- packages/cyberstorm/src/components/Layout/CommunityListLayout/CommunityListSkeleton.tsx
+++ packages/cyberstorm/src/components/Layout/CommunityListLayout/CommunityListSkeleton.tsx
@@ -3,8 +3,8 @@ import styles from "./CommunityListSkeleton.module.css";
 export const CommunityListSkeleton = () => {
   return (
     <div className={styles.root}>
-      {[1, 2, 3, 4].map((community) => (
-        <div key={community} className={styles.card}>{community}</div>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((community) => (
+        <div key={community} className={styles.card}></div>
       ))}
     </div>
   );
