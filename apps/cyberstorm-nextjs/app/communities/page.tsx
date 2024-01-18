2901cde3674c7556725f0e0c9afedc4e72e0eba8
Oksamies <asko.soisalo@gmail.com>
2024-01-18 19:56:03 +0200

    Disable SSR on Communities and index pages

--- apps/cyberstorm-nextjs/app/communities/page.tsx
+++ apps/cyberstorm-nextjs/app/communities/page.tsx
@@ -1,5 +1,21 @@
-import { CommunityListLayout } from "@thunderstore/cyberstorm";
+// Disable SSR for now, until we've split the "Cyberstorm layouts"
+// into actual NextJS layouts
+// Then we can have SSR on some parts of the page
+// There seems to be no way of disabling SSR on certain components,
+// without using nextjs in Cyberstorm library
+
+import dynamic from "next/dynamic";
+
+const DynamicCommunityListLayout = dynamic(
+  () =>
+    import(
+      "@thunderstore/cyberstorm/src/components/Layout/CommunityListLayout/CommunityListLayout"
+    ),
+  {
+    ssr: false,
+  }
+);
 
 export default function Page() {
-  return <CommunityListLayout />;
+  return <DynamicCommunityListLayout />;
 }
