fd45c285783a89c2e40296785b090259f81278fa
Antti Mäki <antti@anttimaki.com>
2024-01-19 10:39:53 +0200

    @thunderstore/cyberstorm: reword comment

--- packages/cyberstorm/src/components/EmptyState/EmptyState.tsx
+++ packages/cyberstorm/src/components/EmptyState/EmptyState.tsx
@@ -7,8 +7,9 @@ interface Props extends PropsWithChildren {
 }
 
 /**
- * Ease of use component set for adding helpful content in
- * place where content is missing
+ * Show fallback content for pages where the content should be fetched
+ * from the database, but matching content doesn't currently exist. Used
+ * e.g. when active filters result in an empty result set.
  */
 export function EmptyState(props: Props) {
   const { children, className } = props;
