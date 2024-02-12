3910f0c87ad588f7d5baae6fa1ce9c12f5d5e6d7
Oksamies <asko.soisalo@gmail.com>
2024-02-12 15:51:35 +0200

    Convert 404 page to NextJS layout

--- apps/cyberstorm-nextjs/app/not-found.tsx
+++ apps/cyberstorm-nextjs/app/not-found.tsx
@@ -1,5 +1,21 @@
-import { ErrorLayout } from "@thunderstore/cyberstorm";
+import rootStyles from "./RootLayout.module.css";
+import errorStyles from "./Error.module.css";
 
 export default function NotFound() {
-  return <ErrorLayout error={404} />;
+  // TODO: Unify and improve errors into a single component
+  return (
+    <section className={rootStyles.content}>
+      <div className={rootStyles.container}>
+        <div className={errorStyles.root}>
+          <div className={errorStyles.glitch} data-text={404}>
+            <span>{404}</span>
+          </div>
+          <div className={errorStyles.description}>Page not found</div>
+          <div className={errorStyles.flavor}>
+            Oops! You found a glitch in the matrix.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
