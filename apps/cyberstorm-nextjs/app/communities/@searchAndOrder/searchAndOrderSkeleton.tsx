2f71134887d9d7d825beb2b4641a623bdf9d0bea
Oksamies <asko.soisalo@gmail.com>
2024-04-09 18:15:45 +0300

    Add missing Skeleton component to communities page

--- apps/cyberstorm-nextjs/app/communities/@searchAndOrder/searchAndOrderSkeleton.tsx
+++ apps/cyberstorm-nextjs/app/communities/@searchAndOrder/searchAndOrderSkeleton.tsx
@@ -1,5 +1,15 @@
+import { SkeletonBox } from "@thunderstore/cyberstorm";
 import styles from "./SearchAndOrder.module.css";
 
 export const SearchAndOrderSkeleton = () => {
-  return <div className={styles.root}>TODO: SKELETON</div>;
+  return (
+    <div className={styles.root}>
+      <div className={styles.searchTextInput}>
+        <SkeletonBox className={styles.skeletonSearch} />
+      </div>
+      <div className={styles.searchFilters}>
+        <SkeletonBox className={styles.skeletonSort} />
+      </div>
+    </div>
+  );
 };
