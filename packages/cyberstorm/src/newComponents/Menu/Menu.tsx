a14c9e2cb85e1b9d671e0bf2e822ce98c0b86f98
Oksamies <asko.soisalo@gmail.com>
2024-09-25 20:08:16 +0300

    Fix issues brought up in QA of PR #1206

--- packages/cyberstorm/src/newComponents/Menu/Menu.tsx
+++ packages/cyberstorm/src/newComponents/Menu/Menu.tsx
@@ -8,15 +8,18 @@ import {
   FramePopoverProps,
   Frame,
 } from "../../primitiveComponents/Frame/Frame";
+import { classnames } from "../../utils/utils";
 
 interface Props extends Omit<FramePopoverProps, "primitiveType"> {
-  trigger: ReactNode;
+  trigger?: ReactNode;
   controls?: ReactNode;
 }
 
 // TODO: Add storybook story
 // TODO: Add support for color, size and text systems
 export function Menu(props: Props) {
+  const { rootClasses } = props;
+
   return (
     <>
       {props.trigger}
@@ -25,28 +28,40 @@ export function Menu(props: Props) {
         popoverId={props.popoverId}
         csColor="pink"
         csSize="m"
-        rootClasses={styles.menuRoot}
-        wrapperClasses={styles.menuWrapper}
+        rootClasses={classnames(styles.menuRoot, rootClasses)}
+        noWrapper
+        popoverMode="manual"
       >
-        {props.controls ? (
-          props.controls
-        ) : (
-          <NewButton
-            {...{
-              popovertarget: props.popoverId,
-              popovertargetaction: "close",
-            }}
-            csVariant="tertiary"
-            className={styles.menuCloseButton}
-            tooltipText="Close"
-          >
-            <NewIcon csMode="inline" noWrapper>
-              <FontAwesomeIcon icon={faXmark} />
-            </NewIcon>
-          </NewButton>
-        )}
-
-        {props.children}
+        <div className={styles.menuWrapper}>
+          {props.controls ? (
+            props.controls
+          ) : (
+            <NewButton
+              {...{
+                popovertarget: props.popoverId,
+                popovertargetaction: "close",
+              }}
+              csVariant="tertiary"
+              className={styles.menuCloseButton}
+              tooltipText="Close"
+              aria-label="Close"
+            >
+              <NewIcon csMode="inline" noWrapper>
+                <FontAwesomeIcon icon={faXmark} />
+              </NewIcon>
+            </NewButton>
+          )}
+          {props.children}
+        </div>
+        <button
+          className={styles.fakeBackdrop}
+          {...{
+            popovertarget: props.popoverId,
+            popovertargetaction: "close",
+          }}
+          aria-hidden
+          tabIndex={-1}
+        />
       </Frame>
     </>
   );
