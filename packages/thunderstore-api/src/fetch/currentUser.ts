0a002227166a1be8beaf18861ee4de980406c72f
Oksamies <asko.soisalo@gmail.com>
2025-04-10 02:34:36 +0300

    Update fetchCurrentUser function to improve type safety by changing return type to CurrentUserResponseData and reorganizing imports for clarity.

--- packages/thunderstore-api/src/fetch/currentUser.ts
+++ packages/thunderstore-api/src/fetch/currentUser.ts
@@ -1,12 +1,14 @@
 import { RequestConfig } from "../index";
 import { apiFetch } from "../apiFetch";
 import { z } from "zod";
-import { currentUserResponseDataSchema } from "../schemas/responseSchemas";
-import { CurrentUser } from "../schemas/objectSchemas";
+import {
+  CurrentUserResponseData,
+  currentUserResponseDataSchema,
+} from "../schemas/responseSchemas";
 
 export async function fetchCurrentUser(
   config: () => RequestConfig
-): Promise<CurrentUser> {
+): Promise<CurrentUserResponseData> {
   const path = "api/experimental/current-user/";
   const request = { cache: "no-store" as RequestCache };
 
