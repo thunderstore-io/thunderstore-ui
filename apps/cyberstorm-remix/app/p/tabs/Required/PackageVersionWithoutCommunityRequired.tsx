850e5ac65e71cb99ee4c2341096b0494335a9f7c
Oksamies <asko.soisalo@gmail.com>
2025-09-19 17:14:33 +0300

    Add PackageVersion pages and tabs (without community)
    These pages are and should be exactly the same, not including community related features and functionalities.
    
    The reason these have to be their own files is that, react-router doesn't support using the same files for two routes at the same time.
    
    The "Required" is mostly commented out, because the endpoint isn't yet available. (even for local development testing)

---
+++ apps/cyberstorm-remix/app/p/tabs/Required/PackageVersionWithoutCommunityRequired.tsx
@@ -0,0 +1,112 @@
+import "./Required.css";
+// import { Heading, SkeletonBox } from "@thunderstore/cyberstorm";
+// import {
+//   Await,
+//   LoaderFunctionArgs
+// } from "react-router";
+// import { useLoaderData, useOutletContext } from "react-router";
+// import { ListingDependency } from "~/commonComponents/ListingDependency/ListingDependency";
+// import { DapperTs } from "@thunderstore/dapper-ts";
+// import { OutletContextShape } from "~/root";
+// import {
+//   getPublicEnvVariables,
+//   getSessionTools,
+// } from "cyberstorm/security/publicEnvVariables";
+// import { Suspense } from "react";
+
+// LOADERS ARE WAITING FOR PACKAGE DEPENDENCIES API ENDPOINT
+
+// export async function loader({ params }: LoaderFunctionArgs) {
+//   if (
+//     params.communityId &&
+//     params.namespaceId &&
+//     params.packageId &&
+//     params.packageVersion
+//   ) {
+//     const publicEnvVariables = getPublicEnvVariables(["VITE_API_URL"]);
+//     const dapper = new DapperTs(() => {
+//       return {
+//         apiHost: publicEnvVariables.VITE_API_URL,
+//         sessionId: undefined,
+//       };
+//     });
+//     return {
+//       listing: dapper.getPackageListingDetails(
+//         params.communityId,
+//         params.namespaceId,
+//         params.packageId
+//       ),
+//     };
+//   }
+//   throw new Response("Listing dependencies not found", { status: 404 });
+// }
+
+// export async function clientLoader({ params }: LoaderFunctionArgs) {
+//   if (
+//     params.communityId &&
+//     params.namespaceId &&
+//     params.packageId &&
+//     params.packageVersion
+//   ) {
+//     const tools = getSessionTools();
+//     const dapper = new DapperTs(() => {
+//       return {
+//         apiHost: tools?.getConfig().apiHost,
+//         sessionId: tools?.getConfig().sessionId,
+//       };
+//     });
+//     return {
+//       listing: dapper.getPackageListingDetails(
+//         params.communityId,
+//         params.namespaceId,
+//         params.packageId
+//       ),
+//     };
+//   }
+//   throw new Response("Listing dependencies not found", { status: 404 });
+// }
+
+export default function PackageVersionRequired() {
+  // const { listing } = useLoaderData<typeof loader | typeof clientLoader>();
+  // const outletContext = useOutletContext() as OutletContextShape;
+
+  return <p>TODO; Waiting for package dependencies API endpoint</p>;
+
+  // return (
+  //   <Suspense fallback={<SkeletonBox className="package-required__skeleton" />}>
+  //     <Await
+  //       resolve={listing}
+  //       errorElement={
+  //         <div>Error occurred while loading required dependencies</div>
+  //       }
+  //     >
+  //       {(resolvedValue) => (
+  //         <>
+  //           <div className="required">
+  //             <div className="required__title">
+  //               <Heading csLevel="3" csSize="3">
+  //                 Required mods ({resolvedValue.dependencies.length})
+  //               </Heading>
+  //               <span className="required__description">
+  //                 This package requires the following packages to work.
+  //               </span>
+  //             </div>
+  //             <div className="required__body">
+  //               {resolvedValue.dependencies.map((dep, key) => {
+  //                 return (
+  //                   <ListingDependency
+  //                     key={key}
+  //                     dependency={dep}
+  //                     // TODO: Remove when package versiond detail is available
+  //                     domain={outletContext.domain}
+  //                   />
+  //                 );
+  //               })}
+  //             </div>
+  //           </div>
+  //         </>
+  //       )}
+  //     </Await>
+  //   </Suspense>
+  // );
+}
