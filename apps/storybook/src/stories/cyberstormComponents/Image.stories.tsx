579bb17401e6e85fcda6e9a25ac1c68d99766858
Oksamies <asko.soisalo@gmail.com>
2025-09-26 23:23:15 +0300

    Add SB story for Image

---
+++ apps/storybook/src/stories/cyberstormComponents/Image.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import "@thunderstore/cyberstorm-theme";
+import { Image } from "@thunderstore/cyberstorm";
+import { ImageVariantsList } from "@thunderstore/cyberstorm-theme/src/components";
+import catHeim from "../assets/catheim.png";
+
+const meta = {
+  title: "Cyberstorm/Image",
+  component: Image,
+  tags: ["autodocs"],
+  argTypes: {
+    csVariant: { control: "select", options: ImageVariantsList },
+    cardType: {
+      control: "select",
+      options: ["community", "communityIcon", "package"],
+    },
+    src: { control: "text" },
+    alt: { control: "text" },
+    square: { control: "boolean" },
+  },
+  args: { src: null, cardType: "community" },
+} satisfies Meta<typeof Image>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+  render: (args) => (
+    <div style={{ width: "300px", height: "400px" }}>
+      <Image {...args} />
+    </div>
+  ),
+};
+export const WithImageAsset: Story = {
+  args: { src: catHeim, alt: "catHeim", cardType: "community" },
+  render: (args) => (
+    <div style={{ width: "150px", height: "200px" }}>
+      <Image {...args} />
+    </div>
+  ),
+};
+export const Community: Story = {
+  args: { alt: "Community", cardType: "community" },
+  render: (args) => (
+    <div style={{ width: "150px", height: "200px" }}>
+      <Image {...args} />
+    </div>
+  ),
+};
+export const CommunityIcon: Story = {
+  args: { alt: "Community", cardType: "communityIcon" },
+  render: (args) => (
+    <div style={{ width: "150px", height: "200px" }}>
+      <Image {...args} />
+    </div>
+  ),
+};
+export const Package: Story = {
+  args: { alt: "Package", cardType: "package" },
+  render: (args) => (
+    <div style={{ width: "150px", height: "200px" }}>
+      <Image {...args} />
+    </div>
+  ),
+};
