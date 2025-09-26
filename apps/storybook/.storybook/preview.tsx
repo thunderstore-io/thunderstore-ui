6414fcc1c68969cddc7352ce806e40a0af8f9bb3
Oksamies <asko.soisalo@gmail.com>
2025-09-26 23:12:59 +0300

    Add new storybook app
    Most of these files are auto-generated by the storybook wizard.
    Non-generated / edited files are
    - .gitignore
    - Dockerfile (from previous app, in case someone uses it)
    - LinkLibrary (for cyberstorm link support)
    - nginx.conf (from previous app, in case someone uses it)
    - preview.tsx (has providers added)
    - styles.css (define layers, so that components work properly)

---
+++ apps/storybook/.storybook/preview.tsx
@@ -0,0 +1,30 @@
+import type { Preview } from "@storybook/react-vite";
+import { LinkingProvider } from "@thunderstore/cyberstorm";
+import "@thunderstore/cyberstorm-theme";
+import { LinkLibrary } from "../LinkLibrary";
+import { Provider as RadixTooltip } from "@radix-ui/react-tooltip";
+import "./styles.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    function (Story) {
+      return (
+        <LinkingProvider value={LinkLibrary}>
+          <RadixTooltip delayDuration={80}>
+            <Story />
+          </RadixTooltip>
+        </LinkingProvider>
+      );
+    },
+  ],
+};
+
+export default preview;
