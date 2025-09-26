a15b794420289bd19fe9cc7004055096989b4bdf
Oksamies <asko.soisalo@gmail.com>
2025-09-27 00:08:15 +0300

    Correct thunderstore-api test config and testData fixtures to match what test CI backend has

--- packages/thunderstore-api/src/__tests__/defaultConfig.ts
+++ packages/thunderstore-api/src/__tests__/defaultConfig.ts
@@ -1,6 +1,6 @@
 export const config = () => {
   return {
-    apiHost: process.env.TEST_API_DOMAIN ?? "https://thunderstore.dev",
+    apiHost: "http://127.0.0.1:8000",
   };
 };
 
@@ -9,7 +9,7 @@ export const config = () => {
  * of the database used by TEST_API_DOMAIN.
  */
 export const testData = {
-  communityId: process.env.TEST_COMMUNITY ?? "test",
-  namespaceId: process.env.TEST_NAMESPACE ?? "Testruction",
-  packageName: process.env.TEST_PACKAGE ?? "Testitute",
+  communityId: "test-community-1",
+  namespaceId: "Test_Team_0",
+  packageName: "Test_Package_0",
 };
