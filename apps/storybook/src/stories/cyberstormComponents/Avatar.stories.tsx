66ea4d2d540b209c2e24a894436ee2cdd72972c7
Oksamies <asko.soisalo@gmail.com>
2025-09-26 23:26:27 +0300

    Add SB story for Avatar

---
+++ apps/storybook/src/stories/cyberstormComponents/Avatar.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import "@thunderstore/cyberstorm-theme";
+import { NewAvatar } from "@thunderstore/cyberstorm";
+import {
+  AvatarSizesList,
+  AvatarVariantsList,
+} from "@thunderstore/cyberstorm-theme/src/components";
+import catboy from "../assets/catboy.png";
+
+const meta = {
+  title: "Cyberstorm/Avatar",
+  component: NewAvatar,
+  tags: ["autodocs"],
+  parameters: { layout: "centered" },
+  argTypes: {
+    csVariant: { control: "select", options: AvatarVariantsList },
+    csSize: { control: "select", options: AvatarSizesList },
+  },
+  args: {
+    csVariant: AvatarVariantsList[0],
+    csSize: AvatarSizesList[1],
+    username: "username",
+    src: null,
+  },
+} satisfies Meta<typeof NewAvatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const WithImage: Story = {
+  args: { src: catboy, username: "CatBoy" },
+};
