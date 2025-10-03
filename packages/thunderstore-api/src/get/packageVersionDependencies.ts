6a1591e6a552f33d40ccdf1b00a9d035806fcd05
Oksamies <asko.soisalo@gmail.com>
2025-10-03 17:09:22 +0300

    Add fetchPackageVersionDependencies function to thunderstore-api
    And the related object schemas
    Additionally the existing PackageDependency type needs to be renamed to
    be more accurate. As it's a package dependency returned with the package
    listing

---
+++ packages/thunderstore-api/src/get/packageVersionDependencies.ts
@@ -0,0 +1,35 @@
+import {
+  ApiEndpointProps,
+  packageVersionDependenciesRequestQueryParamsSchema,
+  PackageVersionDependenciesRequestQueryParams,
+  PackageVersionDependenciesRequestParams,
+  packageVersionDependenciesResponseDataSchema,
+  PackageVersionDependenciesResponseData,
+} from "../index";
+import { apiFetch } from "../apiFetch";
+
+export async function fetchPackageVersionDependencies(
+  props: ApiEndpointProps<
+    PackageVersionDependenciesRequestParams,
+    PackageVersionDependenciesRequestQueryParams,
+    object
+  >
+): Promise<PackageVersionDependenciesResponseData> {
+  const {
+    config,
+    params,
+    queryParams = [{ key: "page", value: 1, impotent: 1 }],
+  } = props;
+  const path = `api/cyberstorm/package/${params.namespace_id}/${params.package_name}/v/${params.version_number}/dependencies/`;
+
+  return await apiFetch({
+    args: {
+      config,
+      path,
+      queryParams,
+    },
+    requestSchema: undefined,
+    queryParamsSchema: packageVersionDependenciesRequestQueryParamsSchema,
+    responseSchema: packageVersionDependenciesResponseDataSchema,
+  });
+}
