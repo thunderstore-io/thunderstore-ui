4c672b192b8b30c7375582dc418c1247f089fbfc
Oksamies <asko.soisalo@gmail.com>
2025-10-10 17:03:19 +0300

    Add rowSemverCompare function for comparing semantic versioning in table rows

--- apps/cyberstorm-remix/cyberstorm/utils/typeChecks.ts
+++ apps/cyberstorm-remix/cyberstorm/utils/typeChecks.ts
@@ -1,3 +1,5 @@
+import semverValid from "semver/functions/valid";
+
 export const isRecord = (obj: unknown): obj is Record<string, unknown> =>
   obj instanceof Object;
 
@@ -5,3 +7,9 @@ export const isRecord = (obj: unknown): obj is Record<string, unknown> =>
 export function isPromise(value: any): value is Promise<unknown> {
   return typeof value?.then === "function";
 }
+
+type ConfirmedSemverStringType = string;
+
+export const isSemver = (s: string): s is ConfirmedSemverStringType => {
+  return Boolean(semverValid(s));
+};
