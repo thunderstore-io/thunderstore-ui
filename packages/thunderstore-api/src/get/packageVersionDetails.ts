4f08f16b3ad1547147789ce706a144c48d7f97f2
Oksamies <asko.soisalo@gmail.com>
2025-10-10 16:55:39 +0300

    Add fetchPackageVersionDetails function and related schemas to thunderstore-api

---
+++ packages/thunderstore-api/src/get/packageVersionDetails.ts
@@ -0,0 +1,24 @@
+import { ApiEndpointProps } from "../index";
+import { apiFetch } from "../apiFetch";
+import { PackageVersionDetailsRequestParams } from "../schemas/requestSchemas";
+import {
+  PackageVersionDetailsResponseData,
+  packageVersionDetailsResponseDataSchema,
+} from "../schemas/responseSchemas";
+
+export async function fetchPackageVersionDetails(
+  props: ApiEndpointProps<PackageVersionDetailsRequestParams, object, object>
+): Promise<PackageVersionDetailsResponseData> {
+  const { config, params } = props;
+  const path = `api/cyberstorm/package/${params.namespace_id}/${params.package_name}/v/${params.package_version}/`;
+
+  return await apiFetch({
+    args: {
+      config: config,
+      path: path,
+    },
+    requestSchema: undefined,
+    queryParamsSchema: undefined,
+    responseSchema: packageVersionDetailsResponseDataSchema,
+  });
+}
