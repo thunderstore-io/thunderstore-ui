15ddefdd89fd08966358e3617df4387de96919ff
Daniel Vainio <daniel.vainio@protonmail.com>
2025-11-14 16:56:06 +0200

    Update Connections.tsx logic
    Update logic for handling removing social auth accounts.
    
    - Use Suspend/Await pattern in component
    - Always fetch new user in clientLoader & update session
    - Revalidate when successful disconnect
    - Use HydrateFallback in order to properly render loading state
    - Keep track of disconnected social auths
    - Prevent user from disconnecting last social auth account
    - Show proper success message to user
    
    Refs. TS-2758

--- apps/cyberstorm-remix/app/settings/user/Connections/Connections.tsx
+++ apps/cyberstorm-remix/app/settings/user/Connections/Connections.tsx
@@ -1,19 +1,30 @@
-import { NewLink, OverwolfLogo, useToast } from "@thunderstore/cyberstorm";
+import { type ReactElement, useRef, Suspense } from "react";
 
-import { useOutletContext } from "react-router";
-import { buildAuthLoginUrl } from "cyberstorm/utils/ThunderstoreAuth";
-
-import { userLinkedAccountDisconnect } from "../../../../../../packages/thunderstore-api/src";
-
-import { Connection } from "~/commonComponents/Connection/Connection";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faDiscord } from "@fortawesome/free-brands-svg-icons";
-import { type ReactElement } from "react";
-import { type OutletContextShape } from "~/root";
+
+import {
+  useOutletContext,
+  useRevalidator,
+  useLoaderData,
+  Await,
+} from "react-router";
+
+import { NewLink, OverwolfLogo, useToast } from "@thunderstore/cyberstorm";
 import { ApiAction } from "@thunderstore/ts-api-react-actions";
-import { NotLoggedIn } from "~/commonComponents/NotLoggedIn/NotLoggedIn";
-import { getPublicEnvVariables } from "cyberstorm/security/publicEnvVariables";
 import { ApiError } from "@thunderstore/thunderstore-api";
+
+import { buildAuthLoginUrl } from "cyberstorm/utils/ThunderstoreAuth";
+import {
+  getPublicEnvVariables,
+  getSessionTools,
+} from "cyberstorm/security/publicEnvVariables";
+
+import { Connection } from "~/commonComponents/Connection/Connection";
+import { NotLoggedIn } from "~/commonComponents/NotLoggedIn/NotLoggedIn";
+import { type OutletContextShape } from "~/root";
+
+import { userLinkedAccountDisconnect } from "../../../../../../packages/thunderstore-api/src";
 
 type ProvidersType = {
   name: string;
@@ -35,27 +46,52 @@ export const PROVIDERS: ProvidersType = [
   { name: "Overwolf", identifier: "overwolf", icon: OverwolfLogo() },
 ];
 
+export function HydrateFallback() {
+  return <div style={{ padding: "32px" }}>Loading...</div>;
+}
+
+export async function clientLoader() {
+  const tools = getSessionTools();
+  const currentUser = tools.getSessionCurrentUser(true);
+  return { currentUser };
+}
+
 export default function Connections() {
   const outletContext = useOutletContext() as OutletContextShape;
-
-  if (!outletContext.currentUser || !outletContext.currentUser.username)
-    return <NotLoggedIn />;
-
+  const { currentUser } = useLoaderData<typeof clientLoader>();
+  const revalidator = useRevalidator();
   const toast = useToast();
+  const disconnectingProviderRef = useRef<string | null>(null);
+
+  const onlyOneConnected = () => {
+    const connectedProviders =
+      outletContext.currentUser?.connections?.length ?? 0;
+    return connectedProviders === 1;
+  };
+
+  const getConnection = (provider: ProvidersType) =>
+    outletContext.currentUser?.connections?.find(
+      (c) => c.provider?.toLowerCase() === provider.identifier
+    );
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const onSubmitSuccess = (_r: unknown) => {
-    if (!outletContext.currentUser || !outletContext.currentUser.username)
-      throw new Error("User not logged in");
+    const username = outletContext.currentUser?.username;
+
+    revalidator.revalidate();
+
     toast.addToast({
       csVariant: "success",
       children: (
         <>
-          User {outletContext.currentUser.username} was disconnected from TODO
+          User {username} was disconnected from{" "}
+          {disconnectingProviderRef.current}
         </>
       ),
       duration: 30000,
     });
+
+    disconnectingProviderRef.current = null;
   };
 
   const onSubmitError = (error: unknown) => {
@@ -90,65 +126,66 @@ export default function Connections() {
   ]);
 
   return (
-    <div className="settings-items">
-      <div className="settings-items__item">
-        <div className="settings-items__meta">
-          <p className="settings-items__title">Connections</p>
-          <p className="settings-items__description">
-            This information will not be shared outside of Thunderstore. Read
-            more on our{" "}
-            <NewLink
-              primitiveType="cyberstormLink"
-              linkId="PrivacyPolicy"
-              csVariant="cyber"
-            >
-              Privacy Policy
-            </NewLink>
-            .
-          </p>
-        </div>
-        <div className="settings-items__content">
-          {PROVIDERS.map((p) => {
-            if (
-              !outletContext.currentUser ||
-              !outletContext.currentUser.username
-            )
-              throw new Error("User not logged in");
-            return (
-              <Connection
-                key={p.name}
-                icon={p.icon}
-                name={p.name}
-                identifier={p.identifier}
-                connection={outletContext.currentUser.connections?.find(
-                  (c) => c.provider.toLowerCase() === p.identifier
-                )}
-                connectionLink={buildAuthLoginUrl({
-                  type: p.identifier,
-                  authBaseDomain: publicEnvVariables.VITE_AUTH_BASE_URL || "",
-                  authReturnDomain:
-                    publicEnvVariables.VITE_AUTH_RETURN_URL || "",
-                })}
-                disconnectFunction={(p) => {
-                  if (
-                    !outletContext.currentUser ||
-                    !outletContext.currentUser.username
-                  )
-                    throw new Error("User not logged in");
-                  return onSubmit({
-                    params: {
-                      provider: p,
-                    },
-                    config: outletContext.requestConfig,
-                    queryParams: {},
-                    data: { provider: p },
-                  });
-                }}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
+    <Suspense key="user-settings-connections">
+      <Await resolve={currentUser} errorElement={<div>Error loading user</div>}>
+        {(resolvedCurrentUser) => {
+          outletContext.currentUser = resolvedCurrentUser;
+
+          if (!outletContext.currentUser) {
+            return <NotLoggedIn />;
+          }
+
+          return (
+            <div className="settings-items">
+              <div className="settings-items__item">
+                <div className="settings-items__meta">
+                  <p className="settings-items__title">Connections</p>
+                  <p className="settings-items__description">
+                    This information will not be shared outside of Thunderstore.
+                    Read more on our{" "}
+                    <NewLink
+                      primitiveType="cyberstormLink"
+                      linkId="PrivacyPolicy"
+                      csVariant="cyber"
+                    >
+                      Privacy Policy
+                    </NewLink>
+                    .
+                  </p>
+                </div>
+                <div className="settings-items__content">
+                  {PROVIDERS.map((provider) => (
+                    <Connection
+                      key={provider.name}
+                      disabled={onlyOneConnected() && !!getConnection(provider)}
+                      icon={provider.icon}
+                      name={provider.name}
+                      identifier={provider.identifier}
+                      connection={getConnection(provider)}
+                      connectionLink={buildAuthLoginUrl({
+                        type: provider.identifier,
+                        authBaseDomain:
+                          publicEnvVariables.VITE_AUTH_BASE_URL || "",
+                        authReturnDomain:
+                          publicEnvVariables.VITE_AUTH_RETURN_URL || "",
+                      })}
+                      disconnectFunction={(provider) => {
+                        disconnectingProviderRef.current = provider;
+                        return onSubmit({
+                          params: { provider },
+                          config: outletContext.requestConfig,
+                          queryParams: {},
+                          data: { provider },
+                        });
+                      }}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </Await>
+    </Suspense>
   );
 }
