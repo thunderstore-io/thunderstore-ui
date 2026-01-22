4c4bb36ccd7adab7a2ac0627d2cc0321640eb2d4
Oksamies <asko.soisalo@gmail.com>
2026-01-22 15:58:22 +0200

    Make rated packages remove session when auth is bad

--- packages/dapper-ts/src/methods/ratedPackages.ts
+++ packages/dapper-ts/src/methods/ratedPackages.ts
@@ -1,14 +1,22 @@
-import { fetchRatedPackages } from "@thunderstore/thunderstore-api";
+import { fetchRatedPackages, isApiError } from "@thunderstore/thunderstore-api";
 
 import type { DapperTsInterface } from "../index";
 
 export async function getRatedPackages(this: DapperTsInterface) {
-  const data = await fetchRatedPackages({
-    config: this.config,
-    params: {},
-    data: {},
-    queryParams: {},
-  });
+  try {
+    const data = await fetchRatedPackages({
+      config: this.config,
+      params: {},
+      data: {},
+      queryParams: {},
+    });
 
-  return data;
+    return data;
+  } catch (error) {
+    if (isApiError(error) && error.response.status === 401) {
+      this.removeSessionHook?.();
+      return { rated_packages: [] };
+    }
+    throw error;
+  }
 }
