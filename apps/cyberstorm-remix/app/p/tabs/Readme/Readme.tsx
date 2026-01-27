944c571264ce460a5ffea4c040b9d332bbd95589
Daniel Vainio <daniel.vainio@protonmail.com>
2026-01-28 00:20:40 +0200

    Catch errors in readme
    - Implement a function for safely fetching readme
    - Remove all other parameters except readme in component
    - Simplify code

--- apps/cyberstorm-remix/app/p/tabs/Readme/Readme.tsx
+++ apps/cyberstorm-remix/app/p/tabs/Readme/Readme.tsx
@@ -8,71 +8,79 @@ import { useLoaderData } from "react-router";
 
 import { SkeletonBox } from "@thunderstore/cyberstorm";
 import { DapperTs } from "@thunderstore/dapper-ts";
+import { isApiError } from "@thunderstore/thunderstore-api";
 
 import "./Readme.css";
 
-export async function loader({ params }: LoaderFunctionArgs) {
-  if (params.namespaceId && params.packageId) {
-    const publicEnvVariables = getPublicEnvVariables(["VITE_API_URL"]);
-    const dapper = new DapperTs(() => {
-      return {
-        apiHost: publicEnvVariables.VITE_API_URL,
-        sessionId: undefined,
-      };
-    });
-    return {
-      readme: dapper.getPackageReadme(params.namespaceId, params.packageId),
-    };
+async function fetchReadmeSafe(
+  dapper: DapperTs,
+  namespaceId: string,
+  packageId: string
+) {
+  try {
+    return await dapper.getPackageReadme(namespaceId, packageId);
+  } catch (error) {
+    if (isApiError(error) && error.response.status === 404) {
+      return null;
+    }
+    throw error;
   }
+}
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  if (!params.namespaceId || !params.packageId) {
+    throw new Response("Not Found", { status: 404 });
+  }
+
+  const publicEnvVariables = getPublicEnvVariables(["VITE_API_URL"]);
+  const dapper = new DapperTs(() => {
+    return {
+      apiHost: publicEnvVariables.VITE_API_URL,
+      sessionId: undefined,
+    };
+  });
+
   return {
-    status: "error",
-    message: "Failed to load readme",
-    readme: { html: "" },
+    readme: await fetchReadmeSafe(dapper, params.namespaceId, params.packageId),
   };
 }
 
 export async function clientLoader({ params }: LoaderFunctionArgs) {
-  if (params.namespaceId && params.packageId) {
-    const tools = getSessionTools();
-    const dapper = new DapperTs(() => {
-      return {
-        apiHost: tools?.getConfig().apiHost,
-        sessionId: tools?.getConfig().sessionId,
-      };
-    });
-    return {
-      readme: dapper.getPackageReadme(params.namespaceId, params.packageId),
-    };
+  if (!params.namespaceId || !params.packageId) {
+    throw new Response("Not Found", { status: 404 });
   }
+
+  const tools = getSessionTools();
+  const dapper = new DapperTs(() => {
+    return {
+      apiHost: tools?.getConfig().apiHost,
+      sessionId: tools?.getConfig().sessionId,
+    };
+  });
+
   return {
-    status: "error",
-    message: "Failed to load readme",
-    readme: { html: "" },
+    readme: fetchReadmeSafe(dapper, params.namespaceId, params.packageId),
   };
 }
 
 export default function Readme() {
-  const { status, message, readme } = useLoaderData<
-    typeof loader | typeof clientLoader
-  >();
+  const { readme } = useLoaderData<typeof loader | typeof clientLoader>();
 
-  if (status === "error") return <div>{message}</div>;
   return (
     <Suspense fallback={<SkeletonBox className="package-readme__skeleton" />}>
-      <Await
-        resolve={readme}
-        errorElement={<div>Error occurred while loading description</div>}
-      >
-        {(resolvedValue) => (
-          <>
+      <Await resolve={readme}>
+        {(resolvedValue) =>
+          resolvedValue ? (
             <div className="markdown-wrapper">
               <div
                 dangerouslySetInnerHTML={{ __html: resolvedValue.html }}
                 className="markdown"
               />
             </div>
-          </>
-        )}
+          ) : (
+            <div>No details available</div>
+          )
+        }
       </Await>
     </Suspense>
   );
