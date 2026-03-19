f38119b37927e4415af1a4ae28951c687515dd94
Oksamies <asko.soisalo@gmail.com>
2026-03-19 21:10:00 +0200

    Feat(Teams): Add clear value button to donation links input (TS-3132)
    Donation Link input was missing the clear value button, so add that bad boy there

--- apps/cyberstorm-remix/app/settings/teams/team/tabs/Profile/Profile.tsx
+++ apps/cyberstorm-remix/app/settings/teams/team/tabs/Profile/Profile.tsx
@@ -166,6 +166,15 @@ function ProfileForm(props: { team: TeamDetails }) {
                   value: e.target.value,
                 })
               }
+              clearValue={
+                formDisabled
+                  ? undefined
+                  : () =>
+                      updateFormFieldState({
+                        field: "donation_link",
+                        value: "",
+                      })
+              }
               rootClasses="team-profile__input"
               disabled={formDisabled}
               {...donationLinkFieldProps}
