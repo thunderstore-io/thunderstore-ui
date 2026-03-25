97d0f664f13d33cc3f664021876b19217e318b5a
Daniel Vainio <daniel.vainio@protonmail.com>
2026-03-25 13:41:59 +0200

    Remove assertTeamAccess
    * Remove assertTeamAccess functions since its not needed anymore
    
    Refs. TS-3040

--- apps/cyberstorm-remix/cyberstorm/utils/permissions.ts
+++ apps/cyberstorm-remix/cyberstorm/utils/permissions.ts
@@ -1,5 +1,3 @@
-import { getSessionTools } from "cyberstorm/security/publicEnvVariables";
-
 import type { CurrentUser } from "@thunderstore/dapper/types";
 
 export const isTeamOwner = (
@@ -7,20 +5,3 @@ export const isTeamOwner = (
   currentUser: CurrentUser | undefined
 ) =>
   currentUser?.teams_full?.find((t) => t.name === teamName)?.role === "owner";
-
-export async function assertTeamAccess(teamName: string) {
-  const tools = getSessionTools();
-  const currentUser = await tools?.getSessionCurrentUser(true);
-
-  if (!currentUser?.username) {
-    throw new Response("Unauthorized", { status: 401 });
-  }
-
-  const hasTeamAccess = (currentUser.teams_full ?? []).some(
-    (team) => team.name === teamName
-  );
-
-  if (!hasTeamAccess) {
-    throw new Response("Forbidden", { status: 403 });
-  }
-}
