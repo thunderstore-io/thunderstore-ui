66c2c7a0e6ea9d95f922fb1a773a1da3e4e27fca
Vili Manninen <vili.manninen@riskidev.fi>
2026-05-27 12:49:07 +0300

    Extract Upload pages Submit section into it's own component

---
+++ apps/cyberstorm-remix/app/upload/components/UploadSubmitSection.tsx
@@ -0,0 +1,62 @@
+import { NewAlert, NewButton } from "@thunderstore/cyberstorm";
+import type { PackageSubmissionStatus } from "@thunderstore/dapper/types";
+
+import { FormSection } from "../../commonComponents/FormSection/FormSection";
+
+export interface UploadSubmitSectionProps {
+  submitError: string | null;
+  strongFormSubmitting: boolean;
+  submissionStatus?: PackageSubmissionStatus;
+  hasSubmissionFormErrors: boolean;
+  submitDisabled: boolean;
+  onReset: () => void;
+  onSubmit: () => void;
+}
+
+export function UploadSubmitSection({
+  submitError,
+  strongFormSubmitting,
+  submissionStatus,
+  hasSubmissionFormErrors,
+  submitDisabled,
+  onReset,
+  onSubmit,
+}: UploadSubmitSectionProps) {
+  return (
+    <FormSection
+      title="Submit"
+      description='Double-check your selections and hit "Submit" when you&apos;re ready!'
+    >
+      {submitError ? (
+        <NewAlert csVariant="danger" rootClasses="upload__alert">
+          {submitError}
+        </NewAlert>
+      ) : null}
+      {!strongFormSubmitting &&
+      submissionStatus?.status !== "PENDING" &&
+      hasSubmissionFormErrors ? (
+        <NewAlert csVariant="danger" rootClasses="upload__alert">
+          Please review the form errors above and submit again.
+        </NewAlert>
+      ) : null}
+      <div className="upload__buttons">
+        <NewButton onClick={onReset} csVariant="secondary" csSize="big">
+          Reset
+        </NewButton>
+        <NewButton
+          disabled={submitDisabled}
+          onClick={onSubmit}
+          csVariant="accent"
+          csSize="big"
+          rootClasses="upload__submit"
+        >
+          {strongFormSubmitting
+            ? "Submitting…"
+            : submissionStatus?.status === "PENDING"
+              ? "Processing…"
+              : "Submit"}
+        </NewButton>
+      </div>
+    </FormSection>
+  );
+}
