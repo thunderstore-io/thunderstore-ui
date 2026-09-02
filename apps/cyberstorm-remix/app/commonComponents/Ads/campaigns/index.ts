7435adc02748642da889a4e143fbfbcf9013aefe
Oksamies <asko.soisalo@gmail.com>
2026-09-02 21:35:26 +0300

    Add the DatHost Valheim launch campaign
    Also moves the existing DatHost affiliate promos onto the same campaign's links
    and copy.

--- apps/cyberstorm-remix/app/commonComponents/Ads/campaigns/index.ts
+++ apps/cyberstorm-remix/app/commonComponents/Ads/campaigns/index.ts
@@ -1,5 +1,6 @@
 import type { AdCampaign } from "../adCampaign";
 import { TEST_CAMPAIGN } from "./testCampaign";
+import { VALHEIM_LAUNCH } from "./valheimLaunch";
 
 // First match wins.
-export const CAMPAIGNS: readonly AdCampaign[] = [TEST_CAMPAIGN];
+export const CAMPAIGNS: readonly AdCampaign[] = [VALHEIM_LAUNCH, TEST_CAMPAIGN];
